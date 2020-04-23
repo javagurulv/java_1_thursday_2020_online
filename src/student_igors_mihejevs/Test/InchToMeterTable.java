8e2da97660be69118763405fea028d17e77b8131
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-04-23 12:10:26 +0300

    Igors Mihejevs first test program

---
+++ src/student_igors_mihejevs/Test/InchToMeterTable.java
@@ -0,0 +1,28 @@
+package student_igors_mihejevs.Test;
+
+/*
+    My first program in Java
+    Task from Java A Beginners Guide, Eighth Edition by Schildt, H. book
+
+    This program displays a conversion
+    table of inches to meters.
+*/
+public class InchToMeterTable {
+    public static void main (String[] args) {
+        double inches, meters, feet;
+        int counter;
+        for (feet = 1; feet <= 12; feet++) {
+            counter = 0;
+            for (inches = 1; inches <= feet * 12; inches++)  {
+                meters = inches * 39.37;
+                System.out.println(inches + " inches is " +
+                                   meters + " meters.");
+                counter++;
+                if (counter == 12) {
+                System.out.println();
+                counter = 0;
+                }
+            }
+        }
+    }
+}
