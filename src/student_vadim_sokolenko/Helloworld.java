5fbd489d6c4d022d5eb7601b8baabf33adb729f2
Денис <deniss.gindins.00@rpg.lv>
2020-04-23 20:38:27 +0200

    Proga

--- src/student_vadim_sokolenko/Helloworld.java
+++ src/student_vadim_sokolenko/Helloworld.java
@@ -4,16 +4,8 @@ public class public class HelloWorld {
     HelloWorld {
 
         public static void main(String[] args) {
-            System.out.println("Hello!");
-            System.out.println("Hello!");
-            System.out.println("Hello!");
-            System.out.println("Hello!");
-            System.out.println("Hello!");
-            System.out.println("Hello!");
-            System.out.println("Hello!");
 
             System.out.println("Hello!");
-
 
         }
 
