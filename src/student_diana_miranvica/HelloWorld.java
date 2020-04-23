9835a9147b40928024c9f83349ef6c57e88cbeea
Диана Миранович <koliti7@inbox.lv>
2020-04-23 19:38:07 +0300

    first commit

---
+++ src/student_diana_miranvica/HelloWorld.java
@@ -0,0 +1,13 @@
+package student_diana_miranvica;
+
+public class HelloWorld {
+   public static void main(String[] args) {
+       System.out.println("Hello!");
+       System.out.println("Hello!");
+       System.out.println("Hello!");
+       System.out.println("Hello!");
+       System.out.println("Hello!");
+       System.out.println("Hello!");
+
+   }
+}
