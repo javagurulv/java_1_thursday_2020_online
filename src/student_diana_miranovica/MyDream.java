82290cb823c5f730504a932360fb60f5e9b7ce8b
Диана Миранович <koliti7@inbox.lv>
2020-04-23 21:52:01 +0300

    dreaming

--- src/student_diana_miranovica/MyDream.java
+++ src/student_diana_miranovica/MyDream.java
@@ -4,5 +4,7 @@ public class MyDream {
     public static void main(String[] args) {
         System.out.print("First");
         System.out.print(" dream!");
+        System.out.print((25 - 17 * 335) * (754 /2 +145));
+        System.out.println("$");
     }
 }
