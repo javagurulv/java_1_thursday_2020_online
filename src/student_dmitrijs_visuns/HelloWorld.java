05bcc26dbdd2d372b30266c776249ca0295e72ba
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-04-23 20:20:24 +0300

    HelloWorld

---
+++ src/student_dmitrijs_visuns/HelloWorld.java
@@ -0,0 +1,20 @@
+package student_dmitrijs_visuns;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+
+        System.out.println("Hello!");
+
+
+    }
+
+
+}
