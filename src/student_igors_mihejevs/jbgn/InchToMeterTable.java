47a88c185d0c656984296a4c6a0f88d7b4503c99
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-04-27 09:27:22 +0300

    Inserting java.text.DecimalFormat

--- src/student_igors_mihejevs/jbgn/InchToMeterTable.java
+++ src/student_igors_mihejevs/jbgn/InchToMeterTable.java
@@ -7,19 +7,29 @@ package student_igors_mihejevs.jbgn;
     This program displays a conversion
     table of inches to meters.
 */
+
+import java.text.DecimalFormat;
+
 class InchToMeterTable {
+
+    private static DecimalFormat df = new DecimalFormat("0.00");
+
     public static void main (String[] args) {
         double inches, meters, feet;
         int counter, counter1;
         inches = 1;
         counter = 0;
+
         for (feet = 1; feet <= 12; feet++) {
             for (counter1 = 1; counter1 <= 12; counter1++) {
             meters = inches * 39.37;
-            System.out.println(inches + " inches is " +
-                    meters + " meters.");
+
+            System.out.println(df.format(inches) + " inches is " +
+                    df.format(meters) + " meters.");
+
             counter++;
             inches = inches + 1;
+
                 if (counter == 12) {
                 System.out.println();
                 counter = 0;
