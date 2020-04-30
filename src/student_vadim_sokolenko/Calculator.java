08104882a1893a2d5408c12ef9edca8f1970320f
vadimsokolenko <vadimsokolenko@gmail.com>
2020-04-30 22:01:37 +0300

    Hello world program!

---
+++ src/student_vadim_sokolenko/Calculator.java
@@ -0,0 +1,21 @@
+package student_vadim_sokolenko;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int numberOne = 10;
+        System.out.println(numberOne);
+
+        numberOne = numberOne + 5;
+        System.out.println(numberOne);
+
+        int numberTwo = 2;
+
+        int result = numberOne + numberTwo;
+
+
+        System.out.println("sum = " + result);
+    }
+
+
+}
