04091dcaf6e18822d5d1f1d423fd4a1ca905660a
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-04-30 21:47:59 +0300

    Inserting java.text.DecimalFormat

--- src/student_igors_mihejevs/lesson2/AddTwoNumbers.java
+++ src/student_igors_mihejevs/lesson2/AddTwoNumbers.java
@@ -8,14 +8,14 @@ public class AddTwoNumbers {
         // 1. get first number from user
         Scanner myInput = new Scanner( System.in );
         System.out.print( "Enter first integer: " );
-        int a = myInput.nextInt();
+        int firstNumber = myInput.nextInt();
 
         // 2. get second number from user
         System.out.print( "Enter second integer: " );
-        int b = myInput.nextInt();
+        int secondNumber = myInput.nextInt();
 
         // 3. first number + second number
-        int result = a + b;
+        int result = firstNumber + secondNumber;
 
         // 4. print result to console
         System.out.println("Result = " + result);
