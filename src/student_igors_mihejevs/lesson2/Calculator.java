4f141fc838e762c6bd34747575b5979252bdc86c
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-04-30 21:45:29 +0300

    Inserting java.text.DecimalFormat

--- src/student_igors_mihejevs/lesson2/Calculator.java
+++ src/student_igors_mihejevs/lesson2/Calculator.java
@@ -6,6 +6,14 @@ public class Calculator {
         int numberOne = 10;
         System.out.println(numberOne);
 
+        numberOne = numberOne + 5;
+        System.out.println(numberOne);
+
+        int numberTwo = 2;
+
+        int result = numberOne + numberTwo;
+        System.out.println("Sum = " + result);
+
 
     }
 }
