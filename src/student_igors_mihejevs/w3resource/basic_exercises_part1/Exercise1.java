d287bef901605836c541fcd2bb6ab130b29ab4c7
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-05-01 21:27:50 +0300

    Test of inputs - string, int

---
+++ src/student_igors_mihejevs/w3resource/basic_exercises_part1/Exercise1.java
@@ -0,0 +1,9 @@
+package student_igors_mihejevs.w3resource.basic_exercises_part1;
+
+// testing \n inside of println
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("Test of Skip inside of println\n\n\nPS\nIt's work fine");
+    }
+}
---
+++ src/student_igors_mihejevs/w3resource/basic_exercises_part1/Exercise2.java
@@ -0,0 +1,19 @@
+package student_igors_mihejevs.w3resource.basic_exercises_part1;
+
+// Sum of two numbers
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        int firstNumber = 74, secondNumber = 36;
+        int result = firstNumber + secondNumber;
+
+        // First variant
+        System.out.print("Result of addition = ");
+        System.out.println(firstNumber+secondNumber);
+
+        System.out.println("------------------------");
+
+        // Second variant
+        System.out.println("Result of addition = " + result);
+    }
+}
