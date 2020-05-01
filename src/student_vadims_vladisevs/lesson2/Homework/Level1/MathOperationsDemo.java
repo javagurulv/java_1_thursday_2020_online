f9add2351adb80f95d3d26fc33c54db65b562118
VadimsVladisevs <64255589+VadimsVladisevs@users.noreply.github.com>
2020-05-01 15:18:38 +0300

    Homework, day 1.

---
+++ src/student_vadims_vladisevs/lesson2/Homework/Level1/MathOperationsDemo.java
@@ -0,0 +1,39 @@
+package student_vadims_vladisevs.lesson2.Homework.Level1;
+import java.util.*;
+
+public class MathOperationsDemo {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Enter first number : ");
+        double firstNumber = scr.nextDouble();
+        System.out.println("Enter second number : ");
+        double secondNumber = scr.nextDouble();
+
+        System.out.println("Choose math operation!");
+        System.out.println("If you want addition enter - 1");
+        System.out.println("If you want subtraction enter - 2");
+        System.out.println("If you want division enter - 3");
+        System.out.println("If you want multiplication enter - 4");
+        int result = scr.nextInt();
+        if ( result == 1){
+            double addResult = firstNumber + secondNumber;
+            System.out.println("Addition result : " + addResult);
+        }
+        else if ( result == 2){
+            double subResult = firstNumber - secondNumber;
+            System.out.println("Subtraction result : " + subResult);
+        }
+        else if (result == 3){
+            double divResult = firstNumber / secondNumber;
+            System.out.println("Division result : " + divResult);
+        }
+        else if (result == 4){
+            double multiResult = firstNumber * secondNumber;
+            System.out.println("Multiplication result : " + multiResult);
+        }
+        else  {
+            System.out.println("Error! Entered number does not meet the conditions!!");
+        }
+
+    }
+}
