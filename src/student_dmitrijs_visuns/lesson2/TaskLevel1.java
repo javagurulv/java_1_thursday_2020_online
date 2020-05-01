10e53e783cf4cc23628fd1167795e428fd4a8f89
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-05-01 19:14:32 +0300

    New

--- src/student_dmitrijs_visuns/lesson2/TaskLevel1.java
+++ src/student_dmitrijs_visuns/lesson2/TaskLevel1.java
@@ -5,23 +5,29 @@ import java.util.Scanner;
 public class TaskLevel1 {
 
         public static void main(String[] args) {
+
             System.out.println("Введите первое целое число");
             Scanner myFirstInput = new Scanner( System.in );
             int firstNumber = myFirstInput.nextInt();
+
             System.out.println("Введите второе целое число");
             Scanner mySecondInput = new Scanner( System.in );
             int secondNumber = mySecondInput.nextInt();
+
             int sum = firstNumber + secondNumber;
             System.out.println("Сумма этих чисел равна " + sum);
+
             int subtract = firstNumber - secondNumber;
             System.out.println("Результат вычитания этих чисел равен " + subtract);
+
             int multiply = firstNumber * secondNumber;
             System.out.println("Результат умножения этих этих чисел равен " + multiply);
+
             int divide = firstNumber / secondNumber;
             System.out.println("Результат деления этих этих чисел равен " + divide);
 
         }
-    }
+}
 
 
 
