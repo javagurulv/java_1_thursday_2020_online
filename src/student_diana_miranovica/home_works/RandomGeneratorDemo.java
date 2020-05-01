6cb34e2f9223d6b9343f7aebbdff6fc33e66983a
Диана Миранович <koliti7@inbox.lv>
2020-05-01 20:54:13 +0300

    home work level 2 lesson 2

---
+++ src/student_diana_miranovica/home_works/RandomGeneratorDemo.java
@@ -0,0 +1,17 @@
+package student_diana_miranovica.home_works;
+
+public class RandomGeneratorDemo {
+    public static void main(String[] args) {
+        double firstNumber = Math.random() * 3;
+
+        System.out.println(firstNumber);
+
+        double secondNumber = 25  + Math.random() * 30;
+
+        System.out.println(secondNumber);
+
+        int thirdNumber = (int) (Math.random() *3);
+
+        System.out.println(thirdNumber);
+    }
+}
