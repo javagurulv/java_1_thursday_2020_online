69afefc70b9280c2a33c091dc00fb7e5b13fec31
Диана Миранович <koliti7@inbox.lv>
2020-05-01 21:05:35 +0300

    home work level 2 lesson 2

--- src/student_diana_miranovica/home_works/RandomGeneratorDemo.java
+++ src/student_diana_miranovica/home_works/RandomGeneratorDemo.java
@@ -1,5 +1,7 @@
 package student_diana_miranovica.home_works;
 
+import java.util.Random;
+
 public class RandomGeneratorDemo {
     public static void main(String[] args) {
         double firstNumber = Math.random() * 3;
@@ -13,5 +15,12 @@ public class RandomGeneratorDemo {
         int thirdNumber = (int) (Math.random() * 3);
 
         System.out.println(thirdNumber);
+
+        Random randomGenerator = new Random();
+        int unboundRandom = randomGenerator.nextInt();
+        int boundRandom = randomGenerator.nextInt( 30);
+        System.out.println("unboundRandom = " + unboundRandom);
+        System.out.println("boundRandom = " + boundRandom);
+
     }
 }
