b0538c30b93c9217ebd80775e64694190062c6d4
julija-sk <j.skopeca@gmail.com>
2020-05-02 18:34:28 +0300

    Tasks form 1 - 6

---
+++ src/student_julija_skopeca/PracticalTasks/Saturday_0205_Task_1_5.java
@@ -0,0 +1,44 @@
+package student_julija_skopeca.PracticalTasks;
+
+//public class Saturday_0205_Task_1 {
+
+    //Задача 1 (level 1):
+    class Book {
+
+    } //добавлена скобка
+
+
+    //Задача 2 (level 1):
+    class Phone{ //добавлена скобка
+
+    }
+
+    //Задача 3 (level 1):
+    class BankAccount {
+
+        public static void main(String[] args) {
+
+
+        }
+    } //добавлена скобка
+
+    //Задача 4 (level 1):
+    class Turtle {
+
+        public static void main(String[] args){ //добавлена скобка
+
+         }
+    }
+
+    //Задача 5 (level 2):
+        class LoginService {
+
+        public static void main(String[] args) { //добавлено слово static
+            System.out.println("You can login!");
+        }
+
+
+    }
+
+
+
