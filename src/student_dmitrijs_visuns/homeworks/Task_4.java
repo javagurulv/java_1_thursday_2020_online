0a587101f01a4065986568fc2a75666097c5834b
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-05-02 19:57:31 +0300

    New

---
+++ src/student_dmitrijs_visuns/homeworks/Task_4.java
@@ -0,0 +1,10 @@
+package student_dmitrijs_visuns.homeworks;
+
+public class Task_4 {
+// Задача 2 (level 1): Дан код, нужно исправить ошибку компиляции.
+
+    class Phone {
+
+    }
+
+}
---
+++ src/student_dmitrijs_visuns/homeworks/Task_7.java
@@ -0,0 +1,11 @@
+package student_dmitrijs_visuns.homeworks;
+// Задача 5 (level 2): Дан код, нужно сделать этот код запускаемым.
+
+public class Task_7 {  //class LoginService
+
+    public static void main(String[] args) {
+
+        System.out.println("You can login!");
+
+    }
+}
