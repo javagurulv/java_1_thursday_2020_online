81e292525e7e7117d2d65674172338496bb424a9
Aleksey Kodin <aleksejs.kodins@gmail.com>
2020-05-02 22:18:43 +0300

    Lesson 3 Level 2

---
+++ src/student_aleksey_kodin/lesson3/level2/EncoderDemoTest.java
@@ -0,0 +1,16 @@
+package student_aleksey_kodin.lesson3.level2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EncoderDemoTest {
+
+    @Test
+    void convertTextToCode() {
+    }
+
+    @Test
+    void convertTextToCodeRecursion() {
+    }
+}
