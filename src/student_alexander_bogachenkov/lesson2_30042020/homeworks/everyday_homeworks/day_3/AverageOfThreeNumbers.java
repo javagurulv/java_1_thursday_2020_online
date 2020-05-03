293bbffe5326fe16717856da793dbe888a91d043
AlexBogachenkov <aleksbogac@gmail.com>
2020-05-03 16:44:36 +0300

    lesson 2 Day_3 tasks added

---
+++ src/student_alexander_bogachenkov/lesson2_30042020/homeworks/everyday_homeworks/day_3/AverageOfThreeNumbers.java
@@ -0,0 +1,16 @@
+package student_alexander_bogachenkov.lesson2_30042020.homeworks.everyday_homeworks.day_3;
+
+import java.util.Scanner;
+
+public class AverageOfThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input first number: ");
+        int firstNum = scanner.nextInt();
+        System.out.println("Input second number: ");
+        int secondNum = scanner.nextInt();
+        System.out.println("Input third number: ");
+        int thirdNum = scanner.nextInt();
+        System.out.println("The average of all these numbers is " + ((firstNum + secondNum + thirdNum) / 3));
+    }
+}
---
+++ src/student_alexander_bogachenkov/lesson2_30042020/homeworks/everyday_homeworks/day_3/Multiplicator.java
@@ -0,0 +1,21 @@
+package student_alexander_bogachenkov.lesson2_30042020.homeworks.everyday_homeworks.day_3;
+
+import java.util.Scanner;
+
+public class Multiplicator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a number: ");
+        int userNum = scanner.nextInt();
+        System.out.println(userNum + " x " + 1 + " = " + (userNum));
+        System.out.println(userNum + " x " + 2 + " = " + (userNum * 2));
+        System.out.println(userNum + " x " + 3 + " = " + (userNum * 3));
+        System.out.println(userNum + " x " + 4 + " = " + (userNum * 4));
+        System.out.println(userNum + " x " + 5 + " = " + (userNum * 5));
+        System.out.println(userNum + " x " + 6 + " = " + (userNum * 6));
+        System.out.println(userNum + " x " + 7 + " = " + (userNum * 7));
+        System.out.println(userNum + " x " + 8 + " = " + (userNum * 8));
+        System.out.println(userNum + " x " + 9 + " = " + (userNum * 9));
+        System.out.println(userNum + " x " + 10 + " = " + (userNum * 10));
+    }
+}
