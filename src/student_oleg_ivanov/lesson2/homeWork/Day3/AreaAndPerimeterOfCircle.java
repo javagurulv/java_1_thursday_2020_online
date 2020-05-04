22f7e19e5e322262b7db672073052975e5afdb56
Oleg617 <oleg.ivanov617@gmail.com>
2020-05-04 22:31:36 +0300

    Home task day 3 an 4

---
+++ src/student_oleg_ivanov/lesson2/homeWork/Day3/AreaAndPerimeterOfCircle.java
@@ -0,0 +1,21 @@
+package student_oleg_ivanov.lesson2.homeWork.Day3;
+
+import java.util.Scanner;
+
+public class AreaAndPerimeterOfCircle {
+    public static void main(String[] args) {
+
+        //get radius number from user
+        System.out.println("Please enter radius of circle:");
+        Scanner myInput = new Scanner(System.in);
+        double circleRadius = myInput.nextDouble();
+
+        //get result area and perimeter of circle
+        double perimeter = circleRadius * Math.PI * 2;
+        double area = circleRadius * circleRadius * Math.PI;
+
+        //give result fot user
+        System.out.println("Perimeter of circle = " + perimeter);
+        System.out.println("Area of circle = " + area);
+    }
+}
