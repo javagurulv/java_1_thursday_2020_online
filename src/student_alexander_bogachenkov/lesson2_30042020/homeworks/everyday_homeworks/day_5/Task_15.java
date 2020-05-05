532453b82e33dc94a9a8e860ecb2a67fd6c00c0b
AlexBogachenkov <aleksbogac@gmail.com>
2020-05-05 20:32:40 +0300

    day_5 tasks added

---
+++ src/student_alexander_bogachenkov/lesson2_30042020/homeworks/everyday_homeworks/day_5/Task_15.java
@@ -0,0 +1,10 @@
+package student_alexander_bogachenkov.lesson2_30042020.homeworks.everyday_homeworks.day_5;
+
+public class Task_15 {
+    public static void main(String[] args) {
+        System.out.print("These");
+        System.out.print("Words");
+        System.out.print("Are");
+        System.out.print("Merged");
+    }
+}
