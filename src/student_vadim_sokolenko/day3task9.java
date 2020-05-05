c0652926f0c4ab4c71d0d456f309478dffb009d5
vadimsokolenko <vadimsokolenko@gmail.com>
2020-05-05 18:50:47 +0300

    Name asking

--- src/student_vadim_sokolenko/day3task9.java
+++ src/student_vadim_sokolenko/day3task9.java
@@ -5,35 +5,16 @@ import java.util.Scanner;
 public class day3task9 {
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter your number: ");
+        int n = s.nextInt();
 
-        //Task. Write a Java program that takes a number as input and prints its
-        //multiplication table up to 10.
-        //
-        //Test Data:
-        //Input a number: 8
-        //
-        //Expected Output :
-        //8 x 1 = 8
-        //8 x 2 = 16
-        //8 x 3 = 24
-        //...
-        //8 x 10 = 80
+        for (int i = 1; i <= 10; i++){
+            System.out.println(n + "*" + i + "=" + n * i);
 
-
-        //1. Ask user the first number
-        Scanner myInput = new Scanner( System.in );
-        int firstNumber = myInput.nextInt();
-
-
-
-
-
-
+        }
 
     }
-
-
-
-
-
 }
+
+
