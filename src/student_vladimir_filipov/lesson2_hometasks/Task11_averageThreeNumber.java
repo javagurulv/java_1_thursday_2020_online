b5507d71e5cd97992b65e50fe9a0f2752d148501
VolodyaF <vladimirs.filipovs@gmail.com>
2020-05-05 20:01:12 +0300

    Tasks9_10_11

---
+++ src/student_vladimir_filipov/lesson2_hometasks/Task11_averageThreeNumber.java
@@ -0,0 +1,23 @@
+package student_vladimir_filipov.lesson2_hometasks;
+
+import java.util.Scanner;
+
+public class Task11_averageThreeNumber {
+
+    public static void main(String[] args) {
+
+        Scanner myInput = new Scanner(System.in);
+
+        System.out.println("Input first number: ");
+        int num1 = myInput.nextInt();
+
+        System.out.println("Input second number: ");
+        int num2 = myInput.nextInt();
+
+        System.out.println("Input third number: ");
+        int num3 = myInput.nextInt();
+
+        System.out.println("The average of all these numbers is " + ((num1 + num2 + num3) / 3));
+    }
+}
+
