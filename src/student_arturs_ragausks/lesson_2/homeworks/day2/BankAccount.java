a3ff223e1fb68562e07ee3d52ab0a8cabeff7a9e
arturas1988 <arturas.arturas88@gmail.com>
2020-05-05 22:02:50 +0300

    Novi Class

---
+++ src/student_arturs_ragausks/lesson_2/homeworks/day2/BankAccount.java
@@ -0,0 +1,8 @@
+package student_arturs_ragausks.lesson_2.homeworks.day2;
+
+public class BankAccount {
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/student_arturs_ragausks/lesson_2/homeworks/day2/GreetingsUser.java
@@ -0,0 +1,19 @@
+package student_arturs_ragausks.lesson_2.homeworks.day2;
+//Ask User for his name-->Get an answer to Greet
+
+import java.util.Scanner;
+
+public class GreetingsUser {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Whats Your Name Mate? ");
+        String userName = scanner.nextLine();
+
+        System.out.println("Greetings " + userName);
+
+
+    }
+}
