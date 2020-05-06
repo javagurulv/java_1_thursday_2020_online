82b47d110ab8a00bb7d0f25b518d7683b0bb39b9
arturas1988 <arturas.arturas88@gmail.com>
2020-05-06 23:08:57 +0300

    Novi Class

---
+++ src/student_arturs_ragausks/lesson_2/homeworks/day3/ThreeNumberAverage.java
@@ -0,0 +1,32 @@
+package student_arturs_ragausks.lesson_2.homeworks.day3;
+
+import java.util.Scanner;
+
+public class ThreeNumberAverage {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("First number : ");
+
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Second number : ");
+
+        int secondNumber = scanner.nextInt();
+
+        System.out.println("Third number ; ");
+
+        int thirdNumber = scanner.nextInt();
+
+        int averageResult = (firstNumber + secondNumber + thirdNumber) / 3;
+
+        System.out.println("Average result : " + averageResult);
+
+
+
+
+    }
+
+
+}
