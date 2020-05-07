8197e0a6e8fcdf919a089478cdaf2ea77f7f5a72
Viktor Savonin <viktor.savonin@meawallet.com>
2020-05-07 21:50:04 +0300

    BankAccount

---
+++ src/teacher/lesson_3/lessoncode/BankAccount.java
@@ -0,0 +1,26 @@
+package teacher.lesson_3.lessoncode;
+
+public class BankAccount {
+
+    private String owner;
+    private int moneyAmount;
+
+    public BankAccount(String owner) {
+        this.owner = owner;
+        this.moneyAmount = 0;
+    }
+
+    public void addMoney(int moneyToAdd) {
+        this.moneyAmount = this.moneyAmount + moneyToAdd;
+    }
+
+    public void removeMoney(int moneyToRemove) {
+        this.moneyAmount = this.moneyAmount - moneyToRemove;
+    }
+
+    public void printStatusToConsole() {
+        System.out.println("Owner = " + owner);
+        System.out.println("Money = " + moneyAmount);
+    }
+
+}
