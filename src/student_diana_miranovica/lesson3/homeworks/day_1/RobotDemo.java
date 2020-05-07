4e0aa3bb8457c4047020015932d33a135efd5efe
Диана Миранович <koliti7@inbox.lv>
2020-05-07 23:46:49 +0300

    home work ))

--- src/student_diana_miranovica/lesson3/homeworks/day_1/RobotDemo.java
+++ src/student_diana_miranovica/lesson3/homeworks/day_1/RobotDemo.java
@@ -1,10 +1,16 @@
 package student_diana_miranovica.lesson3.homeworks.day_1;
 
+import student_diana_miranovica.lesson3.homeworks.day_1.Robot;
+
 public class RobotDemo {
     public static void main(String[] args) {
-        Robot robotic = new Robot();
-        robotic.sayHello();
-        robotic.sayName();
+        Robot robotic1 = new Robot();
+        robotic1.sayHello();
+        robotic1.sayName();
+
+        Robot robotic2 = new Robot();
+        robotic2.sayHello();
+        robotic2.sayName();
     }
 }
 
