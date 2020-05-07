b6bf6d7351a3e588599a2ba7ce83f02175873820
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-05-07 13:41:23 +0300

    Lesson3 Robot and RobotDemo classes

---
+++ src/student_igors_mihejevs/lesson_3/Robot.java
@@ -0,0 +1,16 @@
+package student_igors_mihejevs.lesson_3;
+
+public class Robot {
+
+    public Robot() {
+    // constructor without arguments
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+}
---
+++ src/student_igors_mihejevs/lesson_3/RobotDemo.java
@@ -0,0 +1,10 @@
+package student_igors_mihejevs.lesson_3;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+
+        Robot robot = new Robot();
+        robot.sayHello();
+        robot.sayYourName();
+    }
+}
