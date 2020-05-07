6d640e60ef1ec4a27a6b87f9637f9cacdde621b6
Диана Миранович <koliti7@inbox.lv>
2020-05-07 21:50:30 +0300

    lesson code

---
+++ src/student_diana_miranovica/lesson_3/BankAccountDemo.java
@@ -0,0 +1,25 @@
+package student_diana_miranovica.lesson_3;
+
+public class BankAccountDemo {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount("Diana");
+        int money = bankAccount.addMoney(1000);
+        System.out.println("Money = " + money);
+
+        money = bankAccount.addMoney(2750);
+        System.out.println("Money = " + money);
+
+        money = bankAccount.removeMoney(100);
+        System.out.println("Money = " + money);
+
+        money = bankAccount.removeMoney(1450);
+        System.out.println("Money = " + money);
+
+        bankAccount.printStatusToConsole();
+
+
+
+
+    }
+}
+
