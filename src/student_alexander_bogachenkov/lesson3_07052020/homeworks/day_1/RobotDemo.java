2ff3b9f2c1cfb40ea8dce941aadc3bd60a3adb30
AlexBogachenkov <aleksbogac@gmail.com>
2020-05-08 17:08:58 +0300

    day_1 and day_2 homeworks added

---
+++ src/student_alexander_bogachenkov/lesson3_07052020/homeworks/day_1/RobotDemo.java
@@ -0,0 +1,18 @@
+package student_alexander_bogachenkov.lesson3_07052020.homeworks.day_1;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+        Robot robot1 = new Robot();
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2 = new Robot();
+        robot2.sayHello();
+        robot2.sayYourName();
+
+        Robot robot3 = new Robot();
+        robot3.sayHello();
+        robot3.sayYourName();
+    }
+}
