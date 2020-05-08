ccf14e282d658f6cd6da7b452941e0ebd99996f6
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-05-08 08:21:11 +0300

    Filipova error

---
+++ src/student_igors_mihejevs/lesson_3/day_2/StringLength.java
@@ -0,0 +1,13 @@
+package student_igors_mihejevs.lesson_3.day_2;
+
+public class StringLength {
+
+    public static void main (String[] args) {
+
+        String stringToTest = "Let's count the length of this string";
+        int lengthOfString = stringToTest.length();
+        System.out.println("String lenth is : " + lengthOfString);
+
+    }
+
+}
--- src/student_vladimir_filipov/lesson3/hometasks/Lesson_WirelessLoudspeakerDemo.java
+++ src/student_vladimir_filipov/lesson3/hometasks/Lesson_WirelessLoudspeakerDemo.java
@@ -1,25 +1,23 @@
 package student_vladimir_filipov.lesson3.hometasks;
 
-import student_diana_miranovica.lesson_3.WirelessLoudspeaker;
-
 public class Lesson_WirelessLoudspeakerDemo {
 
     public static void main(String[] args) {
-        student_diana_miranovica.lesson_3.WirelessLoudspeaker ws1 = new student_diana_miranovica.lesson_3.WirelessLoudspeaker("Samsung ",false);
+        Lesson_WirelessLoudspeaker ws1 = new Lesson_WirelessLoudspeaker("Samsung ",false);
         ws1.switchOn();
         ws1.switchOn();
         ws1.switchOff();
         String model = ws1.getModel();
         System.out.println("WS 1 model =" + model);
 
-        student_diana_miranovica.lesson_3.WirelessLoudspeaker ws2 = new student_diana_miranovica.lesson_3.WirelessLoudspeaker("Bose", true);
+        Lesson_WirelessLoudspeaker ws2 = new Lesson_WirelessLoudspeaker("Bose", true);
         ws2.switchOn();
         ws2.switchOff();
         String model2 = ws2.getModel();
         System.out.println("WS 2 model =" + model2);
 
 
-        student_diana_miranovica.lesson_3.WirelessLoudspeaker ws3 = new WirelessLoudspeaker("LG", false);
+        Lesson_WirelessLoudspeaker ws3 = new Lesson_WirelessLoudspeaker("LG", false);
         ws3.switchOn();
         ws3.switchOff();
         String model3 = ws3.getModel();
