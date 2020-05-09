03b20432b4e9d91ea4eb041a9036404cfdae0748
julija-sk <j.skopeca@gmail.com>
2020-05-09 22:44:47 +0300

    day 2 tasks 7-10

---
+++ src/student_julija_skopeca/practical_tasks/lesson_3/day_2/task_8.java
@@ -0,0 +1,10 @@
+package student_julija_skopeca.practical_tasks.lesson_3.day_2;
+
+public class task_8 {
+
+    public static void main(String[] args) {
+
+        String userName = "Julija";
+        System.out.println("Hi "+ userName + "!" );
+    }
+}
