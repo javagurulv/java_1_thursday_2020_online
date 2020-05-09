65ff9a004abbdb946dc9213893b7777e63b6eccb
Diana <63928714+Ciklop-Ciklop@users.noreply.github.com>
2020-05-09 17:21:30 +0300

    home work ))

---
+++ src/student_diana_miranovica/lesson3/homeworks/day_5/task_18/Dog.java
@@ -0,0 +1,16 @@
+package student_diana_miranovica.lesson3.homeworks.day_5.task_18;
+
+public class Dog {
+    private String name;
+
+    public Dog(String dogName){
+        this.name = dogName;
+    }
+
+    public void voice(){
+        System.out.println(this.name);
+        System.out.println(this.name);
+        System.out.println(this.name);
+    }
+
+}
---
+++ src/student_diana_miranovica/lesson3/homeworks/day_5/task_18/DogDemo.java
@@ -0,0 +1,8 @@
+package student_diana_miranovica.lesson3.homeworks.day_5.task_18;
+
+public class DogDemo {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Flafy");
+        dog.voice();
+    }
+}
