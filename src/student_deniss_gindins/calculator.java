8292c13dd8eae522b978a031c80c02ad28bf00ef
Денис <deniss.gindins.00@rpg.lv>
2020-05-09 09:28:48 +0200

    Calculator

---
+++ src/student_deniss_gindins/calculator.java
@@ -0,0 +1,18 @@
+package student_deniss_gindins;
+
+public class calculator {
+
+    public static void main(String[] args) {
+        int numberOne = 10;
+        System.out.println(numberOne);
+
+        numberOne = numberOne + 5;
+        System.out.println(numberOne);
+
+        int numberTwo;
+        numberTwo = 2;
+        int result = numberOne + numberTwo;
+        System.out.println("Sum = " + result);
+
+    }
+}
