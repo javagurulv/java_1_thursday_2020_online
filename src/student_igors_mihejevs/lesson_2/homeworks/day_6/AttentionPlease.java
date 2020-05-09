4a2c023405b974a4138d823103b3041dd9e97011
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-05-09 10:52:20 +0300

    lesson_2 corrections

--- src/student_igors_mihejevs/lesson_2/homeworks/day_6/AttentionPlease.java
+++ src/student_igors_mihejevs/lesson_2/homeworks/day_6/AttentionPlease.java
@@ -4,6 +4,5 @@ public class AttentionPlease {
     public static void main(String[] args) {
 
     System.out.println("\nCode inside the method will compile!");
-
     }
 }
--- src/student_igors_mihejevs/lesson_2/homeworks/day_6/Task_21_1.java
+++ src/student_igors_mihejevs/lesson_2/homeworks/day_6/Task_21_1.java
@@ -2,6 +2,5 @@ package student_igors_mihejevs.lesson_2.homeworks.day_6;
 
 public class Task_21_1 {
     public static void main(String[] args) {
-
     }
 }
