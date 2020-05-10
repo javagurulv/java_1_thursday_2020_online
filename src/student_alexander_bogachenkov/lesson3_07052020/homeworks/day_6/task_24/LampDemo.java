b2b19df692c03b192d24ca51ffa0c68e232ac441
AlexBogachenkov <aleksbogac@gmail.com>
2020-05-10 16:52:40 +0300

    day_6 homeworks added

---
+++ src/student_alexander_bogachenkov/lesson3_07052020/homeworks/day_6/task_24/LampDemo.java
@@ -0,0 +1,11 @@
+package student_alexander_bogachenkov.lesson3_07052020.homeworks.day_6.task_24;
+
+public class LampDemo {
+    public static void main(String[] args) {
+        Lamp lamp1 = new Lamp("red",30,false, true);
+        lamp1.infoAboutLamp();
+
+        Lamp lamp2 = new Lamp("black",45,true, false);
+        lamp2.infoAboutLamp();
+    }
+}
