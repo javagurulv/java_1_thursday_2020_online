291297ee35e6f74f8480a96a772ec7511701c371
VadimsVladisevs <64255589+VadimsVladisevs@users.noreply.github.com>
2020-05-10 19:33:34 +0300

    homework added - lesson3, super_task_1

---
+++ src/student_vadims_vladisevs/lesson3/day_x/super_task_1/User.java
@@ -0,0 +1,49 @@
+package student_vadims_vladisevs.lesson3.day_x.super_task_1;
+
+public class User {
+
+    private String userLogin;
+    private String userPassword;
+    private boolean isUserBlocked;
+    private int loginAttemptsCount;
+
+    public User(String userLogin, String userPassword){
+        this.userLogin = userLogin;
+        this.userPassword = userPassword;
+        this.isUserBlocked = false;
+        this.loginAttemptsCount = 3;
+    }
+
+    public void setLoginAttemptsCount(int count){
+        this.loginAttemptsCount = count;
+    }
+
+    public int getLoginAttemptsCount(){
+        return loginAttemptsCount;
+    }
+
+    public String getUserPassword(){
+        return userPassword;
+    }
+
+    public void setUserBlocked(){
+        isUserBlocked = true;
+    }
+
+    public void resetAttemptsCount(){
+        loginAttemptsCount = 3;
+    }
+
+    public boolean getIsUserBlocked(){
+        return isUserBlocked;
+    }
+
+
+    public void printUserInfo(){
+        System.out.println("User login: " + userLogin);
+        System.out.println("User password: " + userPassword);
+        System.out.println("User status(is user blocked): " + isUserBlocked);
+        System.out.println("Login attempts count: " + loginAttemptsCount);
+    }
+}
+
