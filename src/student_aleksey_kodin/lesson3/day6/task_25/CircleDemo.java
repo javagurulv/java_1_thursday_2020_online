3180578cbc10921df42b1667ebf189cca5277751
Diana <63928714+Ciklop-Ciklop@users.noreply.github.com>
2020-05-10 13:38:45 +0300

    changes

--- src/student_aleksey_kodin/lesson3/day6/task_25/CircleDemo.java
+++ src/student_aleksey_kodin/lesson3/day6/task_25/CircleDemo.java
@@ -5,15 +5,15 @@ public class CircleDemo {
     public static void main(String[] args) {
 
         Circle Circle = new Circle(5.05);
-        System.out.println("Circle Radius - " + Circle.getRadius());
-        System.out.println("Circle Area - " + Circle.getCircleArea());
+        System.out.println("Radius - " + Circle.getRadius());
+        System.out.println("Area - " + Circle.getCircleArea());
 
         Circle.setRadius(10.15);
-        System.out.println("Circle Radius - " + Circle.getRadius());
-        System.out.println("Circle Area - " + Circle.getCircleArea());
+        System.out.println("Radius - " + Circle.getRadius());
+        System.out.println("Area - " + Circle.getCircleArea());
 
         Circle.setRadius(15.25);
-        System.out.println("Circle Radius - " + Circle.getRadius());
-        System.out.println("Circle Area - " + Circle.getCircleArea());
+        System.out.println("Radius - " + Circle.getRadius());
+        System.out.println("Area - " + Circle.getCircleArea());
     }
 }
