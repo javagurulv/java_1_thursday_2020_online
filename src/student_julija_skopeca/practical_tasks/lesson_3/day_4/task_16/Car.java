94032c6fc3433b267a0fb35368a80ad37afdbb80
julija-sk <j.skopeca@gmail.com>
2020-05-10 19:32:48 +0300

    day 4 tasks 14-17

---
+++ src/student_julija_skopeca/practical_tasks/lesson_3/day_4/task_16/Car.java
@@ -0,0 +1,14 @@
+package student_julija_skopeca.practical_tasks.lesson_3.day_4.task_16;
+
+public class Car {
+
+    private String model;
+
+    public Car(String newModel) {
+        this.model = newModel;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
