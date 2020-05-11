8d38d2c7df0e132654ffffcb21aee9a7f77cc897
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-05-11 11:28:21 +0300

    lesson_3 day_5 corrections

--- src/student_igors_mihejevs/lesson_3/day_5/task_22/DogDemo.java
+++ src/student_igors_mihejevs/lesson_3/day_5/task_22/DogDemo.java
@@ -3,18 +3,14 @@ package student_igors_mihejevs.lesson_3.day_5.task_22;
 public class DogDemo {
 
     public static void main(String[] args) {
-        Dog myDog = new Dog();
-        myDog.dogName("Viking");
-        myDog.dogAge(5);
-        myDog.dogColor("Ginger");
+        Dog myDog = new Dog("Viking", 5, "Ginger");
+
         myDog.voice();
-        System.out.println("\nAfter one year");
-        System.out.println("--------------");
+        System.out.println("\nAfter one year :");
         myDog.happyBirthday();
         myDog.voice();
+        System.out.println("\nAfter changing color :");
         myDog.changeColor("Black");
-        System.out.println("\nAfter changing color");
-        System.out.println("--------------");
         myDog.voice();
     }
 
