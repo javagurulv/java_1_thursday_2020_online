e823cd863be73f393ce96e1f9a9a9ec4ee0fccbb
vadimsokolenko <vadimsokolenko@gmail.com>
2020-05-12 00:09:24 +0300

    robot

--- src/student_vadim_sokolenko/lesson3day2task7.java
+++ src/student_vadim_sokolenko/lesson3day2task7.java
@@ -1,12 +1,14 @@
 package student_vadim_sokolenko;
 
+
+
 public class lesson3day2task7 {
 
-    private String name;
-    String myName = Vadim
-
-
-    public lesson3day2task7(String name) {
-        this.name = name;
+    public static void main(String[] args) {
+        String myName = "Vadim";
+        System.out.println(myName);
     }
+
+
 }
+
