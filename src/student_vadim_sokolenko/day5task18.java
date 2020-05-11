3a1116a0196544322e7f9995e9133109f9f4c07f
vadimsokolenko <vadimsokolenko@gmail.com>
2020-05-11 17:11:45 +0300

    I have an errors

---
+++ src/student_vadim_sokolenko/day5task18.java
@@ -0,0 +1,15 @@
+package student_vadim_sokolenko;
+
+public class day5task18 {
+
+    //Заставьте эту программу работать!
+    //
+    //
+
+    public static class SizeMatters {
+
+        public static void main(String[] args) {
+            System.out.println("Sorry, I have an errors.");
+            System.out.println("Please fix me!");
+        }
+    }}
