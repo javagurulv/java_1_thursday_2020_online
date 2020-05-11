32e37b4cdfa7d0b177be45bc60bb22bc1f9a16b0
vadimsokolenko <vadimsokolenko@gmail.com>
2020-05-11 22:39:00 +0300

    robot

--- src/student_vadim_sokolenko/day5task25.java
+++ src/student_vadim_sokolenko/day5task25.java
@@ -4,7 +4,7 @@ public class day5task25 {
 
     public static class NoSleepNoFocus {
         public static void main(String[] args) {
-            System.out.println("I wanna coffee");
+            System.out.print("I wanna coffee ");
 
-            System.out.println("So bad");
+            System.out.print("So bad");
 }}}
---
+++ src/student_vadim_sokolenko/robot.java
@@ -0,0 +1,23 @@
+package student_vadim_sokolenko;
+
+public class robot {
+
+    private String name;
+
+        public robot(String robotName) {
+        this.name = robotName;
+        }
+
+    public robot() {
+
+    }
+
+    public void sayHello() { System.out.println("Hello!"); }
+    public void sayName() {
+            System.out.println("My name is " + this.name);
+    }
+
+
+
+
+}
