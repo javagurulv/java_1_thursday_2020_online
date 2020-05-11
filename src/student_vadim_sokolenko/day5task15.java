8a21e2e294038fcdfd26df4f69dd00535d84f2da
vadimsokolenko <vadimsokolenko@gmail.com>
2020-05-11 15:44:55 +0300

    Average

--- src/student_vadim_sokolenko/day5task15.java
+++ src/student_vadim_sokolenko/day5task15.java
@@ -2,5 +2,13 @@ package student_vadim_sokolenko;
 
 public class day5task15 {
 
+    public static void main(String[] args) {
 
+        //Найти в интернете как выводить на консоль строки без перевода курсора на новую строку.
+        //Написать программу, которая демонстрирует это.
+        System.out.println("Hello World!");
+        System.out.println("Bye World!");
+
+
+    }
 }
