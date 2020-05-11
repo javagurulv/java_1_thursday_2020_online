7a8dca16cdb5fdd07eb0863b9c8ff04974917897
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-05-11 22:59:48 +0300

    New

---
+++ src/student_dmitrijs_visuns/homeworks/lesson_3/day_4/sdf.java
@@ -0,0 +1,4 @@
+package student_dmitrijs_visuns.homeworks.lesson_3.day_4;
+
+public class sdf {
+}
---
+++ src/student_dmitrijs_visuns/homeworks/lesson_3/day_4/task_14/Book.java
@@ -0,0 +1,17 @@
+package student_dmitrijs_visuns.homeworks.lesson_3.day_4.task_14;
+
+public class Book {
+
+        private String title;
+
+        public Book(String bookTitle) {
+            this.title = bookTitle;
+        }
+
+        public String getTitle() {
+            return this.title;
+        }
+
+    }
+
+
