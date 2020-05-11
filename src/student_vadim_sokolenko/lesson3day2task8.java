f90b05c95e42a846b40c5e5fb17a871e05522cf4
vadimsokolenko <vadimsokolenko@gmail.com>
2020-05-12 00:32:31 +0300

    hi

---
+++ src/student_vadim_sokolenko/lesson3day2task8.java
@@ -0,0 +1,12 @@
+package student_vadim_sokolenko;
+
+public class lesson3day2task8 {
+
+    public static void main(String[] args) {
+        String myName = "Vadim";
+        String greeting = "Hi " + myName + "!";
+
+        System.out.println(greeting);
+}}
+
+
