9a8f8854c7f27b2cd5a0a0244a2404d4f257d19d
julija-sk <j.skopeca@gmail.com>
2020-05-12 23:48:23 +0300

    day 5 tasks 18-22

--- src/student_julija_skopeca/practical_tasks/lesson_3/day_5/DogDemo.java
+++ src/student_julija_skopeca/practical_tasks/lesson_3/day_5/DogDemo.java
@@ -1,13 +1,13 @@
-package student_julija_skopeca.practical_tasks.lesson_3.day_5.task_18;
+package student_julija_skopeca.practical_tasks.lesson_3.day_5;
 
  class DogDemo {
 
     public static void main(String[] args) {
 
         Dog pet = new Dog ("Baton", 29, "Red");
-            //pet.voiceName();
+            pet.voiceName();
             pet.voiceCharacteristics();
-            //pet.happyBirthday();
+            pet.happyBirthday();
             pet.changeColor();
             pet.voiceCharacteristics();
     }
