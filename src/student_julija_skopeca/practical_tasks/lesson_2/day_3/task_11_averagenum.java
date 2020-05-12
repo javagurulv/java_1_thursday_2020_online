34993235b6a10b73038a9e2278fe8bcf4d98e587
Diana <63928714+Ciklop-Ciklop@users.noreply.github.com>
2020-05-12 22:34:30 +0300

    public delete

--- src/student_julija_skopeca/practical_tasks/lesson_2/day_3/task_11_averagenum.java
+++ src/student_julija_skopeca/practical_tasks/lesson_2/day_3/task_11_averagenum.java
@@ -1,8 +1,8 @@
-package student_julija_skopeca.practical_tasks.day_3;
+package student_julija_skopeca.practical_tasks.lesson_2.day_3;
 
 import java.util.Scanner;
 
-public class task_11_averagenum {
+ class task_11_averagenum {
 
     public static void main(String[] args) {
 
