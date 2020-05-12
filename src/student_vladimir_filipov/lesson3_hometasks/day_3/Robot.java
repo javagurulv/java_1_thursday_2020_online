e36abfe05fe5cdb7d342b312e787dcd03e8cafad
VolodyaF <vladimirs.filipovs@gmail.com>
2020-05-12 15:37:24 +0300

    Lesson3_Hometasks_day3_4

---
+++ src/student_vladimir_filipov/lesson3_hometasks/day_3/Robot.java
@@ -0,0 +1,16 @@
+package student_vladimir_filipov.lesson3_hometasks.day_3;
+
+public class Robot {
+
+        private final String name;
+
+        public Robot(String robotName) {
+                this.name = robotName;
+        }
+
+        public void sayHello() { System.out.println("Hello!"); }
+        public void sayYourName() { System.out.println("My name is - " + this.name);
+
+        }
+
+}
