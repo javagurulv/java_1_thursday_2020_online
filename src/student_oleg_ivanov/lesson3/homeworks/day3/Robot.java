92d5bf1aa2762f67cdb1e984c67630dd65837bc5
Oleg617 <oleg.ivanov617@gmail.com>
2020-05-12 06:54:55 +0300

    Lesson 3 Day3

---
+++ src/student_oleg_ivanov/lesson3/homeworks/day3/Robot.java
@@ -0,0 +1,12 @@
+package student_oleg_ivanov.lesson3.homeworks.day3;
+
+public class Robot {
+    private String name;
+    public Robot(String Robotname) {this.name = Robotname; }
+
+    public void sayHello() { System.out.println("Hello "); }
+
+    public void sayYourName() {System.out.println("My Name is " + name + "!"); }
+
+
+}
