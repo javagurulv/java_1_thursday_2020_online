5f279b2e6dff423f08d8945a51021a1e28358e1c
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-05-12 22:21:32 +0300

    public deleted Filipov

--- src/student_vladimir_filipov/lesson2_hometasks/Task25_noSleepNoFocus.java
+++ src/student_vladimir_filipov/lesson2_hometasks/Task25_noSleepNoFocus.java
@@ -1,6 +1,6 @@
 package student_vladimir_filipov.lesson2_hometasks;
 
-public class Task25_noSleepNoFocus {
+class Task25_noSleepNoFocus {
 
     public static void main(String[] args) {
         System.out.println("I wanna coffee");
--- src/student_vladimir_filipov/lesson3_hometasks/day_2/Task_8.java
+++ src/student_vladimir_filipov/lesson3_hometasks/day_2/Task_8.java
@@ -1,6 +1,6 @@
 package student_vladimir_filipov.lesson3_hometasks.day_2;
 
-public class Task_8 {
+class Task_8 {
 
     public static void main(String[] args) {
         String myName = "Vladimir";
