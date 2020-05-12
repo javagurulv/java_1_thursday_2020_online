4929971e8c32690ad8e2c6e8dae903a083ef3c95
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-05-12 15:06:02 +0300

    lesson_3 day_x super_task_1

---
+++ src/student_igors_mihejevs/lesson_3/day_x/super_task_1/UserLoginService.java
@@ -0,0 +1,16 @@
+package student_igors_mihejevs.lesson_3.day_x.super_task_1;
+
+public class UserLoginService {
+
+    public boolean login(User user, String password) {
+
+        if(password.equals(user.getPassword())) {
+            user.cancelNumberOfAttempts();
+            return true;
+        } else {
+            user.decreaseNumberOfAttempts();
+            user.userIsBlocked();
+            return false;
+        }
+    }
+}
---
+++ src/student_igors_mihejevs/lesson_3/day_x/super_task_1/UserLoginServiceDemo.java
@@ -0,0 +1,25 @@
+package student_igors_mihejevs.lesson_3.day_x.super_task_1;
+
+public class UserLoginServiceDemo {
+
+    public static void main(String[] args) {
+        UserLoginService service = new UserLoginService();
+        User userOne = new User("user-login", "user-password");
+
+        System.out.println("User Login = " + userOne.getLoginName() + "\nUser Password = " + userOne.getPassword()
+                           + "\nNumber of attempts = " + userOne.getNumberOfAttempts());
+
+        if(!userOne.isBlocked()) {
+            if(!service.login(userOne, "user-password-12345")) {
+                System.out.println("Password 'user-password-12345' is not correct"); //Should return false
+            }
+            if(service.login(userOne, "user-password")) {
+                System.out.println("Password 'user-password' is correct"); //Should return true
+            }
+        } else {
+            System.out.println("You are blocked. Waiting for unblocking.");
+        }
+
+
+    }
+}
