67f7b762b1014be0256df1fe6ffe1277980a6cf0
vadimsokolenko <vadimsokolenko@gmail.com>
2020-05-12 16:24:36 +0300

    hi

---
+++ src/student_vadim_sokolenko/userWordAskingProgram.java
@@ -0,0 +1,24 @@
+package student_vadim_sokolenko;
+
+
+import java.util.Scanner;
+
+public class userWordAskingProgram {
+
+             public static void main(String[] args) {
+
+
+                //1.ask user a name
+                 Scanner myInput = new Scanner(System.in);
+                 System.out.println("What is your name?");
+
+
+                String name = myInput.nextLine();
+
+                 //2.print hello and user name
+
+                 System.out.println(" Hello! " + name);
+
+
+             }
+}
