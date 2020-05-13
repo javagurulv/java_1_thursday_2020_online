242066873792bf470777acaa56d102b4ebefa739
AlexBogachenkov <aleksbogac@gmail.com>
2020-05-13 17:27:46 +0300

    tasks 27 & 28 added

---
+++ src/student_alexander_bogachenkov/lesson3_07052020/homeworks/day_6/task_27/BankAccount.java
@@ -0,0 +1,21 @@
+package student_alexander_bogachenkov.lesson3_07052020.homeworks.day_6.task_27;
+
+class BankAccount {
+
+    private String owner;
+    private int money;
+
+    public BankAccount(String owner, int moneyAmount) {
+        this.owner = owner;
+        this.money = moneyAmount;
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+
+    public int getMoney() {
+        return this.money;
+    }
+
+}
