3e4250083a6e95cebe6f939e3cafcb286ad050fe
vadimsokolenko <vadimsokolenko@gmail.com>
2020-05-13 13:06:50 +0300

    hi

--- src/student_vadim_sokolenko/Dog.java
+++ src/student_vadim_sokolenko/Dog.java
@@ -1,7 +1,24 @@
 package student_vadim_sokolenko;
 
+import java.util.Scanner;
+
 public class Dog {
 
     private String name;
 
+    public Dog(String dogName){
+        this.name = dogName;}
+
+
+
+    public void sayName(){
+        System.out.println(this.name);
+        System.out.println(this.name);
+        System.out.println(this.name);
+
+    }
 }
+
+
+
+
---
+++ src/student_vadim_sokolenko/DogDemo.java
@@ -0,0 +1,17 @@
+package student_vadim_sokolenko;
+
+import java.util.Scanner;
+
+public class DogDemo {
+
+    public static void main(String[] args) {
+
+        Scanner myInput = new Scanner(System.in);
+        System.out.println("voice!");
+
+        Dog myDog = new Dog("Jack");
+        myDog.sayName();
+
+
+    }
+}
