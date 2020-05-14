d679ca8cc0798215f6276c47e888b432259ce32e
vsavonins <viktors.savonins@proofit.lv>
2020-05-14 22:02:27 +0300

    Lesson 4

--- src/teacher/lesson_4/lessoncode/Calculator.java
+++ src/teacher/lesson_4/lessoncode/Calculator.java
@@ -2,10 +2,9 @@ package teacher.lesson_4.lessoncode;
 
 class Calculator {
 
-
-	public int sum(int firstNumber, int secondNumber) {
+	public int sum(int firstNumber,
+				   int secondNumber) {
 		return firstNumber + secondNumber;
 	}
-
 
 }
