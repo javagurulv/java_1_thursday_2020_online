fa51ab17b730c5e2706d8b1c98931326621bea66
julija-sk <j.skopeca@gmail.com>
2020-05-15 21:02:54 +0300

    day 6 tasks 23-24

---
+++ src/student_julija_skopeca/practical_tasks/lesson_3/day_6/task_24/GlassDemo.java
@@ -0,0 +1,13 @@
+package student_julija_skopeca.practical_tasks.lesson_3.day_6.task_24;
+
+class GlassDemo {
+
+    public static void main(String[] args) {
+
+        Glass newGlass = new Glass("Goble", 500,"None");
+        newGlass.getShapeVolume();
+        newGlass.getTitle();
+        newGlass.getFullInfo();
+
+        }
+}
