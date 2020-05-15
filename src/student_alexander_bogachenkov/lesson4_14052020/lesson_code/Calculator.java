62dff9b43a78c9e4b1cbce35af4d045ea2bd5cfe
AlexBogachenkov <aleksbogac@gmail.com>
2020-05-15 17:26:09 +0300

    lesson code added

---
+++ src/student_alexander_bogachenkov/lesson4_14052020/lesson_code/Calculator.java
@@ -0,0 +1,10 @@
+package student_alexander_bogachenkov.lesson4_14052020.lesson_code;
+
+class Calculator {
+
+	public int sum(int firstNumber,
+				   int secondNumber) {
+		return firstNumber + secondNumber;
+	}
+
+}
