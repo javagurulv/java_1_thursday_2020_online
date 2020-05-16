717f4b540bb526df473dc9d9ec971c914ceaba2e
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-05-16 21:10:58 +0300

    corrections

--- src/student_igors_mihejevs/lesson_4/day_x/LeapYear.java
+++ src/student_igors_mihejevs/lesson_4/day_x/LeapYear.java
@@ -1,10 +1,8 @@
 package student_igors_mihejevs.lesson_4.day_x;
 
 public class LeapYear {
-    //int year;
 
     public boolean yearDetect(int number) {
-        //this.year = number;
 
         if (number % 4 == 0) {
             if (number % 100 != 0) {
