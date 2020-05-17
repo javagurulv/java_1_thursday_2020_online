b627d70e8539125f0fb374d31c8680f9e03a2ace
julija-sk <j.skopeca@gmail.com>
2020-05-17 23:14:30 +0300

    day 1 tasks 1-4

---
+++ src/student_julija_skopeca/practical_tasks/lesson_4/CalculatorTest.java
@@ -0,0 +1,40 @@
+package student_julija_skopeca.practical_tasks.lesson_4;
+
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+        CalculatorTest calculatorTest = new CalculatorTest();
+        test1();
+    }
+
+        //test 1: f = -1; s = -3; r = -13
+        public static void  test1() {
+          int firstnumber = -10;
+          int secondnumber = -3;
+          Calculator calculator = new Calculator();
+          int actualresult = calculator.sum(firstnumber, secondnumber);
+
+          if (actualresult == -13) {
+              System.out.println("Test 1 = ok");
+          } else {
+              System.out.println("Test 1 = fail");
+          }
+
+        }
+        //test 2: f = 0; s= 0;  r = 0
+        public static void  test2() {
+
+
+        }
+        //test 3:
+        public static void  test3() {
+
+
+        }
+        //test 4:
+        public static void  test4() {
+
+
+        }
+    }
+
