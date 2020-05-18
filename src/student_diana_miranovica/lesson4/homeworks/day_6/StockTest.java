c53c1650c3456bef3538f5f2124dc5a43c49f9bb
Diana <63928714+Ciklop-Ciklop@users.noreply.github.com>
2020-05-18 22:29:01 +0300

    home work

---
+++ src/student_diana_miranovica/lesson4/homeworks/day_6/StockTest.java
@@ -0,0 +1,8 @@
+package student_diana_miranovica.lesson4.homeworks.day_6;
+
+public class StockTest {
+    public static void main(String[] args) {
+        StockTest stockTest = new StockTest();
+
+    }
+}
