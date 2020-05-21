5e35493e781977b869232e273ee12aa69679cb98
AlexBogachenkov <aleksbogac@gmail.com>
2020-05-21 22:01:55 +0300

    lesson_code added

---
+++ src/student_alexander_bogachenkov/lesson5_21052020/lesson_code/BankAccount.java
@@ -0,0 +1,12 @@
+package student_alexander_bogachenkov.lesson5_21052020.lesson_code;
+
+class BankAccount {
+
+    private String owner;
+    private int money;
+
+    public BankAccount(String owner) {
+        this.owner = owner;
+        this.money = 0;
+    }
+}
