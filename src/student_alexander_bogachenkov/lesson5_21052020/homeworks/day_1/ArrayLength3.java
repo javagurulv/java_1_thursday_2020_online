9c20523824a992ad6113f395db205419e5366337
AlexBogachenkov <aleksbogac@gmail.com>
2020-05-22 15:52:38 +0300

    day_1, tasks 7-9 added

---
+++ src/student_alexander_bogachenkov/lesson5_21052020/homeworks/day_1/ArrayLength3.java
@@ -0,0 +1,10 @@
+package student_alexander_bogachenkov.lesson5_21052020.homeworks.day_1;
+
+class ArrayLength3 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers.length);
+    }
+
+}
