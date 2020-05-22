3900a1ba1b0220df0e9dd31b2e6567d13a335310
Oleg617 <oleg.ivanov617@gmail.com>
2020-05-22 12:43:40 +0300

    Lesson5 Day 1

---
+++ src/student_oleg_ivanov/lesson5/homeTaskDay1/ArrayDefinition2.java
@@ -0,0 +1,7 @@
+package student_oleg_ivanov.lesson5.homeTaskDay1;
+
+class ArrayDefinition2 {
+    public static void main(String[] args) {
+        int[] numbers = new int[10]; // скобки после типа данных, разные название типа данных.
+    }
+}
---
+++ src/student_oleg_ivanov/lesson5/homeTaskDay1/ArrayDefinition3.java
@@ -0,0 +1,8 @@
+package student_oleg_ivanov.lesson5.homeTaskDay1;
+
+class ArrayDefinition3 {
+    public static void main(String[] args) {
+        boolean[] numbers = new boolean[10]; // скобки после типа данных, разные название типа данных.
+    }
+
+}
