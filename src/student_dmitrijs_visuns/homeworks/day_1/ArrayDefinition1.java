85c6f58745b5318454a1df98256b63988482d9fd
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-05-23 20:18:07 +0300

    New

---
+++ src/student_dmitrijs_visuns/homeworks/day_1/ArrayDefinition1.java
@@ -0,0 +1,10 @@
+package student_dmitrijs_visuns.homeworks.day_1;
+
+class ArrayDefinition1 {
+
+        public static void main(String[] args) {
+
+            int[] numbers = new int[] {};
+        }
+
+}
---
+++ src/student_dmitrijs_visuns/homeworks/day_1/ArrayDefinition3.java
@@ -0,0 +1,11 @@
+package student_dmitrijs_visuns.homeworks.day_1;
+
+//Найдите ошибку в программе и исправить её:
+
+class ArrayDefinition3 {
+
+    public static void main(String[] args) {
+        boolean[] numbers = new boolean[10];
+    }
+
+}
