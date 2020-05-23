2eb2c59cfda8be32bc04fc24bc5052b539dbc192
Diana <63928714+Ciklop-Ciklop@users.noreply.github.com>
2020-05-23 22:59:30 +0300

    home work

--- src/student_diana_miranovica/lesson5/homeworks/day_3/ForLoopDefinition7.java
+++ src/student_diana_miranovica/lesson5/homeworks/day_3/ForLoopDefinition7.java
@@ -1,6 +1,6 @@
 package student_diana_miranovica.lesson5.homeworks.day_3;
 
-public class ForLoopDefinition7 {
+class ForLoopDefinition7 {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
