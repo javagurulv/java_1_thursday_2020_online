d887576ae1c7f538528a04dae4db764ba8b51c69
julija-sk <j.skopeca@gmail.com>
2020-05-24 14:54:11 +0300

    lesson 5 day 3 tasks 16 - 24

---
+++ src/student_julija_skopeca/practical_tasks/lesson_5/task_3/task_20.java
@@ -0,0 +1,10 @@
+package student_julija_skopeca.practical_tasks.lesson_5.task_3;
+
+class task_20 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
