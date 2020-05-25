acdecb426441a11af71df6c8908dd07450ff63a0
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-05-25 20:11:17 +0300

    New

---
+++ src/student_dmitrijs_visuns/homeworks/lesson_5/day_3/ForLoopDefinition2.java
@@ -0,0 +1,11 @@
+package student_dmitrijs_visuns.homeworks.lesson_5.day_3;
+
+class ForLoopDefinition2 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
