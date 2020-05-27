55a0cd1e6cc9ae4e969f2fa7b8c0ef57f0c0c2e6
vadimsokolenko <vadimsokolenko@gmail.com>
2020-05-27 18:46:09 +0300

    Array

---
+++ src/student_vadim_sokolenko/Lesson5HomeWorks/Day3/ForLoopDefinition.java
@@ -0,0 +1,10 @@
+package student_vadim_sokolenko.Lesson5HomeWorks.Day3;
+
+public class ForLoopDefinition {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/student_vadim_sokolenko/Lesson5HomeWorks/Day3/ForLoopDefinition3.java
@@ -0,0 +1,12 @@
+package student_vadim_sokolenko.Lesson5HomeWorks.Day3;
+
+public class ForLoopDefinition3 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
+
