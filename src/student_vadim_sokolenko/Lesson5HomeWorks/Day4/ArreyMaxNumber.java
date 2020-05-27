ddd0d1a793dcecf5f6a22d22a1503341e377d892
vadimsokolenko <vadimsokolenko@gmail.com>
2020-05-27 19:57:23 +0300

    Arrays

---
+++ src/student_vadim_sokolenko/Lesson5HomeWorks/Day4/ArreyMaxNumber.java
@@ -0,0 +1,33 @@
+package student_vadim_sokolenko.Lesson5HomeWorks.Day4;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArreyMaxNumber {
+
+    public static void main(String[] args) {
+
+        int[] number = new int[10];
+        Random random = new Random();
+
+
+        for (int i = 0; i < number.length; i++) {
+            int randomNumber = random.nextInt(10);
+            number[i] = randomNumber;
+        }
+        System.out.println("The array is " + Arrays.toString(number));
+
+        int maxNumber = 0;
+        for (int i = 0; i < number.length; i++){
+            if (maxNumber <= number[i]){
+                maxNumber = number[i];
+            }
+
+        }
+
+        System.out.println("The maximal number in the array is " + maxNumber);
+        }
+    }
+
+
+
