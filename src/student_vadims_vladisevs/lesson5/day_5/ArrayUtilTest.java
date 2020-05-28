a2574abd33011a2ab3923dcbbf4f8ba7e4201cb4
VadimsVladisevs <vadims.vladisevs@gmail.com>
2020-05-28 22:29:24 +0300

    added lesson 5 upd

--- src/student_vadims_vladisevs/lesson5/day_5/ArrayUtilTest.java
+++ src/student_vadims_vladisevs/lesson5/day_5/ArrayUtilTest.java
@@ -6,6 +6,7 @@ class ArrayUtilTest {
         ArrayUtilTest test = new ArrayUtilTest();
         test.shouldCreateArray();
         test.shouldFillArrayWithRandomNumbers();
+        test.shouldFillArrayWithRandomNumbersV2();
     }
 
     public void shouldCreateArray() {
@@ -32,7 +33,30 @@ class ArrayUtilTest {
         printResult(0, failCount);
     }
 
+    public void shouldFillArrayWithRandomNumbersV2() {
+        // Write test implementation here !!!
+        ArrayUtil tempArray = new ArrayUtil();
+        int[] testArray = tempArray.createArray(10);
+        tempArray.fillArrayWithRandomNumbers(testArray);
+
+        int sum = 0;
+        for (int temp : testArray){
+            sum += temp;
+        }
+
+        printResult((sum > 0), (sum > 0));
+    }
+
     public void printResult(int expectedResult, int result){
+        String testName = Thread.currentThread().getStackTrace()[2].getMethodName();
+        if (result == expectedResult){
+            System.out.println(testName + " test = OK");
+        } else {
+            System.out.println(testName + " test = FAIL");
+        }
+    }
+
+    public void printResult(boolean expectedResult, boolean result){
         String testName = Thread.currentThread().getStackTrace()[2].getMethodName();
         if (result == expectedResult){
             System.out.println(testName + " test = OK");
