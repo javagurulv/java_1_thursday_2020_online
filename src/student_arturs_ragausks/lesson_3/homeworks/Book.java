4cf4798d7f9747fea3a8358c904d3d41c7f0eb13
artur <arturas.arturas88@gmail.com>
2020-05-28 18:52:03 +0200

    student_arturs_ragausks.lesson_3.homeworks.day4;TaskBook student_arturs_ragausks.lesson_3.homeworks.day4;TaskBookDemo

---
+++ src/student_arturs_ragausks/lesson_3/homeworks/Book.java
@@ -0,0 +1,15 @@
+package student_arturs_ragausks.lesson_3.homeworks;
+
+public class Book {
+
+    private String title;
+
+    public Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+}
