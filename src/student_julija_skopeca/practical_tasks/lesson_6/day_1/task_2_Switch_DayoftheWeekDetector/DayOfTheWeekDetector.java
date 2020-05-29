cf1f3125e1ef2e0526b1aa97c913edcbbeec6f4c
julija-sk <j.skopeca@gmail.com>
2020-05-29 23:15:05 +0300

    lesson 6 day 1 tasks 1-2

---
+++ src/student_julija_skopeca/practical_tasks/lesson_6/day_1/task_2_Switch_DayoftheWeekDetector/DayOfTheWeekDetector.java
@@ -0,0 +1,18 @@
+package student_julija_skopeca.practical_tasks.lesson_6.day_1.task_2_Switch_DayoftheWeekDetector;
+
+class DayOfTheWeekDetector {
+
+    public String detectDayName(int firstNumber) {
+         if ((firstNumber >=1) && (firstNumber <= 7)) {
+            switch (firstNumber) {
+                case 1 -> {return "Monday";}
+                case 2 -> {return "Tuesday";}
+                case 3 -> {return "Wednesday";}
+                case 4 -> {return "Thursday";}
+                case 5 -> {return "Friday";}
+                case 6 -> {return "Saturday";}
+                case 7 -> {return "Sunday";}
+            }
+        } return "Please input a valid number between 1 and 7";
+   }
+}
