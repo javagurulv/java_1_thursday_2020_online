e2a7df70d3294344fca14999e78b68140e6aa4c3
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-06-01 20:49:56 +0300

    Day 3 tasks 12 & 13

--- src/student_dmitrijs_visuns/homeworks/lesson_6/day_3/ArrayUtilTest.java
+++ src/student_dmitrijs_visuns/homeworks/lesson_6/day_3/ArrayUtilTest.java
@@ -9,6 +9,8 @@ class ArrayUtilTest {
         newTest.findNumberTest(); // Task_9
         newTest.howManyTimesIsNumberInArrayTest(); // Task_10
         newTest.replaceNumberInArrayTest(); // Task_11
+        newTest.replaceAllTest(); // Task_12
+        newTest.reverseArrayNumbersTest(); // Task_13
 
     }
 
@@ -37,11 +39,33 @@ class ArrayUtilTest {
         ArrayUtil arrayUtil = new ArrayUtil();
         int[] array = new int[] {12, 5, 9, 12, 12};
         arrayUtil.replaceNumberInArray(array, 12, 3);
-        System.out.println(array[0]);
         if (array[0] == 3) {
             System.out.println("Replace number in array test - OK");
         } else {
             System.out.println("Replace number in array test - FAIL");
         }
     }
+
+    public void replaceAllTest() {
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] array = new int[] {12, 5, 9, 12, 12};
+        arrayUtil.replaceAll(array, 12, 3);
+        if ((array[0] == 3) && (array[3] == 3) && (array[4] == 3)) {
+            System.out.println("Replace all given numbers in array test - OK");
+        } else {
+            System.out.println("Replace all given numbers in array test - FAIL");
+        }
+    }
+
+    public void reverseArrayNumbersTest() {
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] array = new int[] {1, 2, 3};
+        arrayUtil.reverseArrayNumbers(array);
+       if ((array[0] == 3) && (array[1] == 2) && (array[2] == 1)) {
+            System.out.println("Reverse array numbers test - OK");
+        } else {
+            System.out.println("Reverse array numbers test - FAIL");
+        }
+    }
+
 }
