eb8efc9c7f7e38cb09ca236b5d1b6fe22b9e69b3
AlexBogachenkov <aleksbogac@gmail.com>
2020-06-01 15:11:50 +0300

    lesson 6, day 5, task 20 added

--- src/student_alexander_bogachenkov/lesson6_28052020/homeworks/day_5/TicTacToeTest.java
+++ src/student_alexander_bogachenkov/lesson6_28052020/homeworks/day_5/TicTacToeTest.java
@@ -7,6 +7,9 @@ class TicTacToeTest {
         test.isWinPositionForFirstHorizontal();
         test.isWinPositionForSecondHorizontal();
         test.isWinPositionForThirdHorizontal();
+        test.isWinPositionForFirstVertical();
+        test.isWinPositionForSecondVertical();
+        test.isWinPositionForThirdVertical();
     }
 
     public void isWinPositionForFirstHorizontal() {
@@ -54,6 +57,54 @@ class TicTacToeTest {
             System.out.println("isWinPositionForThirdHorizontal PASSED");
         } else {
             System.out.println("isWinPositionForThirdHorizontal FAILED");
+        }
+    }
+
+    public void isWinPositionForFirstVertical() {
+        int[][] field = new int[3][3];
+        TicTacToe ticTacToe = new TicTacToe();
+        field[0][0] = 0;
+        field[1][0] = 0;
+        field[2][0] = 0;
+        boolean expectedResult = true;
+        boolean actualResult = ticTacToe.isWinPositionForHorizontals(field,0);
+
+        if (actualResult == expectedResult) {
+            System.out.println("isWinPositionForFirstVertical PASSED");
+        } else {
+            System.out.println("isWinPositionForFirstVertical FAILED");
+        }
+    }
+
+    public void isWinPositionForSecondVertical() {
+        int[][] field = new int[3][3];
+        TicTacToe ticTacToe = new TicTacToe();
+        field[0][1] = 0;
+        field[1][1] = 0;
+        field[2][1] = 0;
+        boolean expectedResult = true;
+        boolean actualResult = ticTacToe.isWinPositionForHorizontals(field,0);
+
+        if (actualResult == expectedResult) {
+            System.out.println("isWinPositionForSecondVertical PASSED");
+        } else {
+            System.out.println("isWinPositionForSecondVertical FAILED");
+        }
+    }
+
+    public void isWinPositionForThirdVertical() {
+        int[][] field = new int[3][3];
+        TicTacToe ticTacToe = new TicTacToe();
+        field[0][2] = 0;
+        field[1][2] = 0;
+        field[2][2] = 0;
+        boolean expectedResult = true;
+        boolean actualResult = ticTacToe.isWinPositionForHorizontals(field,0);
+
+        if (actualResult == expectedResult) {
+            System.out.println("isWinPositionForThirdVertical PASSED");
+        } else {
+            System.out.println("isWinPositionForThirdVertical FAILED");
         }
     }
 }
