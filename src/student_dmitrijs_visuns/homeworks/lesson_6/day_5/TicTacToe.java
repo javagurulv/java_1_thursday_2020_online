7c88973d65a1c17825f2acecac72da46e2520636
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-06-03 00:04:38 +0300

    lesson 6 TicTacToe tasks (in progress)

--- src/student_dmitrijs_visuns/homeworks/lesson_6/day_5/TicTacToe.java
+++ src/student_dmitrijs_visuns/homeworks/lesson_6/day_5/TicTacToe.java
@@ -27,34 +27,56 @@ class TicTacToe {
     }
 
     public boolean isWinPositionForHorizontals(int[][] field, int playerToCheck) {
-        if ((field [0][0] == playerToCheck) && (field [0][1] == playerToCheck) && (field [0][2] == playerToCheck)) {
+        if (field [0][0] == playerToCheck &&
+            field [0][1] == playerToCheck &&
+            field [0][2] == playerToCheck) {
             return true;
-        } else if ((field [1][0] == playerToCheck) && (field [1][1] == playerToCheck) && (field [1][2] == playerToCheck)) {
+        } else if (field [1][0] == playerToCheck &&
+                   field [1][1] == playerToCheck &&
+                   field [1][2] == playerToCheck) {
             return true;
-        } else if ((field [2][0] == playerToCheck) && (field [2][1] == playerToCheck) && (field [2][2] == playerToCheck)) {
+        } else if (field [2][0] == playerToCheck &&
+                   field [2][1] == playerToCheck &&
+                   field [2][2] == playerToCheck) {
             return true;
         }
         else return false;
     }
 
     public boolean isWinPositionForVerticals(int[][] field, int playerToCheck) {
-        if ((field [0][0] == playerToCheck) && (field [1][0] == playerToCheck) && (field [2][0] == playerToCheck)) {
+        if (field [0][0] == playerToCheck &&
+            field [1][0] == playerToCheck &&
+            field [2][0] == playerToCheck) {
             return true;
-        } else if ((field [0][1] == playerToCheck) && (field [1][1] == playerToCheck) && (field [2][1] == playerToCheck)) {
+        } else if (field [0][1] == playerToCheck &&
+                   field [1][1] == playerToCheck &&
+                   field [2][1] == playerToCheck) {
             return true;
-        } else if ((field [0][2] == playerToCheck) && (field [1][2] == playerToCheck) && (field [2][2] == playerToCheck)) {
+        } else if (field [0][2] == playerToCheck &&
+                   field [1][2] == playerToCheck &&
+                   field [2][2] == playerToCheck) {
             return true;
         }
         else return false;
     }
 
     public boolean isWinPositionForDiagonals(int[][] field, int playerToCheck) {
-        if ((field [0][0] == playerToCheck) && (field [1][1] == playerToCheck) && (field [2][2] == playerToCheck)) {
+        if (field [0][0] == playerToCheck &&
+            field [1][1] == playerToCheck &&
+            field [2][2] == playerToCheck) {
             return true;
         } else if ((field [2][0] == playerToCheck) && (field [1][1] == playerToCheck) && (field [0][2] == playerToCheck)) {
             return true;
         }
         else return false;
+    }
+
+    public boolean isWinPosition(int[][] field, int playerToCheck) {
+        if (isWinPositionForHorizontals(field, playerToCheck) ||
+            isWinPositionForVerticals(field, playerToCheck) ||
+            isWinPositionForDiagonals(field, playerToCheck)) {
+            return true;
+        } else { return false; }
     }
 
 
