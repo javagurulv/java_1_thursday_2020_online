5b4b8fc134b1ec74fb7db568baeea5600316d9cc
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-06-02 23:11:59 +0300

    lesson 6 TicTacToe tasks (in progress)

---
+++ src/student_dmitrijs_visuns/homeworks/lesson_6/day_5/TicTacToe.java
@@ -0,0 +1,41 @@
+package student_dmitrijs_visuns.homeworks.lesson_6.day_5;
+
+class TicTacToe {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] createField() {
+        int[][] field = new int[3][3];
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                field[i][j] = -1;
+            }
+        }
+        return field;
+    }
+
+    public void printFieldToConsole(int[][] field) {
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                System.out.print(field[i][j] + " ");
+            }
+            System.out.println("");
+        }
+
+    }
+
+    public boolean isWinPositionForHorizontals(int[][] field, int playerToCheck) {
+        if ((field [0][0] == playerToCheck) && (field [0][1] == playerToCheck) && (field [0][2] == playerToCheck)) {
+            return true;
+        } else if ((field [1][0] == playerToCheck) && (field [1][1] == playerToCheck) && (field [1][2] == playerToCheck)) {
+            return true;
+        } else if ((field [2][0] == playerToCheck) && (field [2][1] == playerToCheck) && (field [2][2] == playerToCheck)) {
+            return true;
+        }
+        else return false;
+    }
+
+
+}
