42765f29abf061a6b6ec586292aec773bb2cd9e7
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-06-02 23:20:58 +0300

    lesson 6 TicTacToe tasks (in progress)

--- src/student_dmitrijs_visuns/homeworks/lesson_6/day_5/TicTacToe.java
+++ src/student_dmitrijs_visuns/homeworks/lesson_6/day_5/TicTacToe.java
@@ -37,5 +37,27 @@ class TicTacToe {
         else return false;
     }
 
+    public boolean isWinPositionForVerticals(int[][] field, int playerToCheck) {
+        if ((field [0][0] == playerToCheck) && (field [1][0] == playerToCheck) && (field [2][0] == playerToCheck)) {
+            return true;
+        } else if ((field [0][1] == playerToCheck) && (field [1][1] == playerToCheck) && (field [2][1] == playerToCheck)) {
+            return true;
+        } else if ((field [0][2] == playerToCheck) && (field [1][2] == playerToCheck) && (field [2][2] == playerToCheck)) {
+            return true;
+        }
+        else return false;
+    }
+
+    public boolean isWinPositionForDiagonals(int[][] field, int playerToCheck) {
+        if ((field [0][0] == playerToCheck) && (field [1][1] == playerToCheck) && (field [2][2] == playerToCheck)) {
+            return true;
+        } else if ((field [2][0] == playerToCheck) && (field [1][1] == playerToCheck) && (field [0][2] == playerToCheck)) {
+            return true;
+        }
+        else return false;
+    }
+
+
+
 
 }
