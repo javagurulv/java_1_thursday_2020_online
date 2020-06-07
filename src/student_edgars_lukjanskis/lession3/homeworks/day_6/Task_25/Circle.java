04538e11de3bd47da5d2c41b10c8d99ed756241d
EL-Projects <64143348+EL-Projects@users.noreply.github.com>
2020-06-07 13:49:25 +0300

    Some homeworks

---
+++ src/student_edgars_lukjanskis/lession3/homeworks/day_6/Task_25/Circle.java
@@ -0,0 +1,16 @@
+package student_edgars_lukjanskis.lession3.homeworks.day_6.Task_25;
+
+class Circle {
+
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+
+    }
+
+    public void calculateArea() {
+        System.out.println("Circle area " + 3.14 * (radius * radius));
+
+    }
+}
