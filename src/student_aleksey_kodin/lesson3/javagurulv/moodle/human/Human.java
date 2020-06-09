6a1784afd2fee8bdc341da930b2cc51fc56afcfd
Aleksey Kodin <aleksejs.kodins@gmail.com>
2020-06-09 17:09:10 +0300

    Lesson 8 all tasks

--- src/student_aleksey_kodin/lesson3/javagurulv/moodle/human/Human.java
+++ src/student_aleksey_kodin/lesson3/javagurulv/moodle/human/Human.java
@@ -4,7 +4,7 @@ class Human {
     private final String name;
     private final int age;
 
-    public Human(String name, int age) {
+    Human(String name, int age) {
         this.name = name;
         this.age = age;
     }
