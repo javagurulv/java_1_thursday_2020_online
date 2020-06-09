2544785fd7babfa8b381efe28ab06f69c2f01ef2
AlexBogachenkov <aleksbogac@gmail.com>
2020-06-09 20:05:21 +0300

    lesson 7, day 6, task 11 added

---
+++ src/student_alexander_bogachenkov/lesson7_04062020/homeworks/day_6/task_11/CreditCardTest.java
@@ -0,0 +1,56 @@
+package student_alexander_bogachenkov.lesson7_04062020.homeworks.day_6.task_11;
+
+import java.math.BigDecimal;
+
+class CreditCardTest {
+
+    public static void main(String[] args) {
+        CreditCardTest test = new CreditCardTest();
+        test.shouldCreateCard();
+        test.shouldDeposit();
+        test.shouldWithdraw();
+    }
+
+    public void shouldCreateCard() {
+        CreditCard creditCard = new CreditCard("1111222233334444", 1234);
+
+        String expectedNumber = "1111222233334444";
+        int expectedPinCode = 1234;
+        String actualNumber = creditCard.getNumber();
+        int actualPinCode = creditCard.getPinCode();
+        if ((expectedNumber.equals(actualNumber)) && (expectedPinCode == actualPinCode)) {
+            System.out.println("shouldCreateCard PASSED");
+        } else {
+            System.out.println("shouldCreateCard FAILED");
+        }
+    }
+
+    public void shouldDeposit() {
+        CreditCard creditCard = new CreditCard("1111222233334444", 1234);
+        creditCard.deposit(1234, new BigDecimal("66.00"));
+
+        BigDecimal expectedBalance = new BigDecimal("66.00");
+        BigDecimal actualBalance = creditCard.getBalance();
+        int res = expectedBalance.compareTo(actualBalance);
+        if (res == 0) {
+            System.out.println("shouldDeposit PASSED");
+        } else {
+            System.out.println("shouldDeposit FAILED");
+        }
+    }
+
+    public void shouldWithdraw() {
+        CreditCard creditCard = new CreditCard("1111222233334444", 1234);
+        creditCard.deposit(1234, new BigDecimal("66.00"));
+        creditCard.withdraw(1234, new BigDecimal("30.00"));
+
+        BigDecimal expectedBalance = new BigDecimal("36.00");
+        BigDecimal actualBalance = creditCard.getBalance();
+        int res = expectedBalance.compareTo(actualBalance);
+        if (res == 0) {
+            System.out.println("shouldWithdraw PASSED");
+        } else {
+            System.out.println("shouldWithdraw FAILED");
+        }
+    }
+}
