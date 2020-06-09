a82638553dd359ba117595fa347607c2cd1ee648
vadimsokolenko <vadimsokolenko@gmail.com>
2020-06-10 00:50:48 +0300

    Arrays

--- src/student_vadim_sokolenko/Lesson5HomeWorks/Day5/ArrayUtil.java
+++ src/student_vadim_sokolenko/Lesson5HomeWorks/Day5/ArrayUtil.java
@@ -24,31 +24,39 @@ public class ArrayUtil {
         // Write implementation here !!!
         int maxNumber = array[0];
         for (int i = 0; i < array.length; i++) {
-            if (maxNumber <= array[i]) {
+            if (maxNumber < array[i]) {
                 maxNumber = array[i];
             }
         }
         return maxNumber;
 
     }
-            public void printArrayToConsole (int[] array) {
-                for (int i = 0; i < array.length; i++) {
-                    System.out.println("array[" + i + "]" + "  in an array = " + array[i]);
-                }
+
+    public void printArrayToConsole(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("array[" + i + "]" + "  in an array = " + array[i]);
+        }
+    }
+
+
+    public int findMinNumber(int[] array) {
+        // Write implementation here !!!
+        int minNumber = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (minNumber > array[i]) {
+                minNumber = array[i];
             }
-
-
-                public int findMinNumber(int[] array) {
-                    // Write implementation here !!!
-                    int minNumber = array [0];
-                    for (int i = 0; i < array.length; i++) {
-                        if (minNumber >= array[i]) {
-                            minNumber = array[i];
-                        }
-                    }
-                    return minNumber;
+        }
+        return minNumber;
     }
 }
+
+
+
+
+
+
+
 
 
 
---
+++ src/student_vadim_sokolenko/Lesson6HomeWorks/Day2/WhileDefinition5.java
@@ -0,0 +1,18 @@
+package student_vadim_sokolenko.Lesson6HomeWorks.Day2;
+
+public class WhileDefinition5 {
+
+
+
+
+        public static void main(String[] args) {
+            int i = 1;
+            while (i <= 100) {
+                System.out.println(i);
+                i++;
+            }
+        }
+
+    }
+
+
