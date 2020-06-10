e21a0dca95ae6f715cd105c034c204787d1bb734
vadimsokolenko <vadimsokolenko@gmail.com>
2020-06-11 00:03:37 +0300

    Arrays

---
+++ src/student_vadim_sokolenko/lesson5Code/ForExample.java
@@ -0,0 +1,38 @@
+package student_vadim_sokolenko.lesson5Code;
+
+class ForExample {
+
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        System.out.println(i);
+        i++;
+
+
+        System.out.println(i);
+        i++;
+
+
+        System.out.println(i);
+        i++;
+
+
+        System.out.println(i);
+        i++;
+
+        for (int j = 1; j < 100; j++) {
+            System.out.println(j);
+
+        }
+
+        for (int j = 100; j>0; j--){
+            System.out.println(j);
+        }
+
+
+        System.out.println("First command after for");
+    }
+}
+
