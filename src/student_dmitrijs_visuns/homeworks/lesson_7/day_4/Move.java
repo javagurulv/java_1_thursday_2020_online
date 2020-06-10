553f054fd2d4be92d4bcc468eda5ab49fab68125
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-06-10 18:36:49 +0300

    Lesson 7 day 4 task

---
+++ src/student_dmitrijs_visuns/homeworks/lesson_7/day_4/Move.java
@@ -0,0 +1,21 @@
+package student_dmitrijs_visuns.homeworks.lesson_7.day_4;
+
+class Move {
+
+    private int x;
+    private int y;
+
+    public Move(int coordinateX, int coordinateY) {
+        this.x = coordinateX;
+        this.y = coordinateY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
