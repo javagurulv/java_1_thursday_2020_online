57cf2664754fb4cb7ad785c183606510e9f3d17a
vadimsokolenko <vadimsokolenko@gmail.com>
2020-06-11 21:18:15 +0300

    Arrays

--- src/student_julija_skopeca/practical_tasks/lesson_6/day_1/task_2_Switch_DayoftheWeekDetector/DayOfTheWeekDetector.java
+++ src/student_julija_skopeca/practical_tasks/lesson_6/day_1/task_2_Switch_DayoftheWeekDetector/DayOfTheWeekDetector.java
@@ -1,18 +1,21 @@
-package student_julija_skopeca.practical_tasks.lesson_6.day_1.task_2_Switch_DayoftheWeekDetector;
-
-class DayOfTheWeekDetector {
-
-    public String detectDayName(int firstNumber) {
-         if ((firstNumber >=1) && (firstNumber <= 7)) {
-            switch (firstNumber) {
-                case 1 -> {return "Monday";}
-                case 2 -> {return "Tuesday";}
-                case 3 -> {return "Wednesday";}
-                case 4 -> {return "Thursday";}
-                case 5 -> {return "Friday";}
-                case 6 -> {return "Saturday";}
-                case 7 -> {return "Sunday";}
-            }
-        } return "Please input a valid number between 1 and 7";
-   }
-}
+//package student_julija_skopeca.practical_tasks.lesson_6.day_1.task_2_Switch_DayoftheWeekDetector;
+//
+//class DayOfTheWeekDetector {
+//
+//    public String detectDayName(int firstNumber) {
+//         if ((firstNumber >=1) && (firstNumber <= 7)) {
+//            switch (firstNumber) {
+//                case 1 -> {return "Monday";}
+//                case 2 -> {return "Tuesday";}
+//                case 3 -> {return "Wednesday";}
+//                case 4 -> {return "Thursday";}
+//                case 5 -> {return "Friday";}
+//                case 6 -> {return "Saturday";}
+//                case 7 -> {return "Sunday";}
+//                default:
+//
+//            }
+//        } return "Please input a valid number between 1 and 7";
+//   }
+//}
+//
