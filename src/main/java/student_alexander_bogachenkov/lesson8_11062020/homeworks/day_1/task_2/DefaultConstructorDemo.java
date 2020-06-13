de213d88d5ab51e54a664a6d553417f2c1a41fe5
AlexBogachenkov <aleksbogac@gmail.com>
2020-06-13 14:02:50 +0300

    lesson 8, day 1, task 2 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson8_11062020/homeworks/day_1/task_2/DefaultConstructorDemo.java
@@ -0,0 +1,15 @@
+package student_alexander_bogachenkov.lesson8_11062020.homeworks.day_1.task_2;
+
+class DefaultConstructorDemo {
+
+    public static void main(String[] args) {
+
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
+
+        //Ошибку не выдает, значит компиляция проходит успешно,
+        //и JAVA автоматически добавляет конструктор при
+        //компиляции кода.
+
+    }
+
+}
