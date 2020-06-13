0b9a331486940e9aa09f2006543a2cbde2889d61
AlexBogachenkov <aleksbogac@gmail.com>
2020-06-13 14:16:12 +0300

    lesson 8, day 1, task 3 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson8_11062020/homeworks/day_1/task_3/Person.java
@@ -0,0 +1,9 @@
+package student_alexander_bogachenkov.lesson8_11062020.homeworks.day_1.task_3;
+
+class Person {
+
+    private String fullName;
+    private int age;
+    private Address address;
+
+}
