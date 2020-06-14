da34e557b3aeccb108cd0bbcab6432cc25567c3a
AlexBogachenkov <aleksbogac@gmail.com>
2020-06-14 18:36:23 +0300

    lesson 8, day 3, task 14 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson8_11062020/homeworks/day_3/task_14/Car.java
@@ -0,0 +1,28 @@
+package student_alexander_bogachenkov.lesson8_11062020.homeworks.day_3.task_14;
+
+abstract class Car {
+
+    abstract void accelerate();
+
+}
+
+class ElectricCar extends Car {
+
+    private int batteryCharge;
+
+
+    @Override
+    void accelerate() {
+
+    }
+}
+
+class PetrolCar extends Car {
+
+    private double petrolTankCapacity;
+
+    @Override
+    void accelerate() {
+
+    }
+}
