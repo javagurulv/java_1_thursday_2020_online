6efc12d40fcfbd30b176e168990d274abb74f9e1
AlexBogachenkov <aleksbogac@gmail.com>
2020-06-19 19:10:07 +0300

    lesson 9, level 1, tasks added

---
+++ src/main/java/student_alexander_bogachenkov/lesson9_18062020/homeworks/level_1/PrivateClass.java
@@ -0,0 +1,14 @@
+package student_alexander_bogachenkov.lesson9_18062020.homeworks.level_1;
+
+class PrivateClass {
+
+    //В Java объявить класс и сделать его приватным,
+    //не видимым для всего другого кода нельзя, так как
+    //модификатор доступа private сам по себе подразумевает
+    //сокрытие логики внутри класса. Было бы бесполезно
+    //скрывать класс внутри класса.
+
+    private String title;
+    private String author;
+
+}
