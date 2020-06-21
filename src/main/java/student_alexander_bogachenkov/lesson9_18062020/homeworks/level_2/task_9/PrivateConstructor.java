e6ad7c0681fb5b662ca36498f1919fd602ceec7b
AlexBogachenkov <aleksbogac@gmail.com>
2020-06-21 13:43:04 +0300

    lesson 9, level 2, task 9 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson9_18062020/homeworks/level_2/task_9/PrivateConstructor.java
@@ -0,0 +1,9 @@
+package student_alexander_bogachenkov.lesson9_18062020.homeworks.level_2.task_9;
+
+class PrivateConstructor {
+
+    private PrivateConstructor() {
+
+    }
+
+}
---
+++ src/main/java/student_alexander_bogachenkov/lesson9_18062020/homeworks/level_2/task_9/PrivateConstructorDemo.java
@@ -0,0 +1,11 @@
+package student_alexander_bogachenkov.lesson9_18062020.homeworks.level_2.task_9;
+
+class PrivateConstructorDemo {
+
+    public static void main(String[] args) {
+
+//        PrivateConstructor privateConstructor = new PrivateConstructor();
+
+    }
+
+}
