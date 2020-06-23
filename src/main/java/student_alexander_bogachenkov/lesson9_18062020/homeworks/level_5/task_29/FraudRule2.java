c3fc7862af1a968431e2b1eda5accb62c1934225
AlexBogachenkov <aleksbogac@gmail.com>
2020-06-23 17:46:10 +0300

    lesson 9, level 5, task 29 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson9_18062020/homeworks/level_5/task_29/FraudRule2.java
@@ -0,0 +1,13 @@
+package student_alexander_bogachenkov.lesson9_18062020.homeworks.level_5.task_29;
+
+class FraudRule2 extends FraudRule {
+
+    public FraudRule2(String ruleName) {
+        super(ruleName);
+    }
+
+    public boolean isFraud(Transaction t) {
+        return t.getAmount() > 1000000;
+    }
+
+}
