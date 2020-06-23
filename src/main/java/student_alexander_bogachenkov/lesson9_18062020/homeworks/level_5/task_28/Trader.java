20bd9b547143e60662e4719ac8631f446f38ccc0
AlexBogachenkov <aleksbogac@gmail.com>
2020-06-23 16:44:02 +0300

    lesson 9, level 5, task 28 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson9_18062020/homeworks/level_5/task_28/Trader.java
@@ -0,0 +1,26 @@
+package student_alexander_bogachenkov.lesson9_18062020.homeworks.level_5.task_28;
+
+class Trader {
+
+    private String fullName;
+    private String country;
+    private String city;
+
+    public Trader(String fullName, String country, String city) {
+        this.fullName = fullName;
+        this.country = country;
+        this.city = city;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
