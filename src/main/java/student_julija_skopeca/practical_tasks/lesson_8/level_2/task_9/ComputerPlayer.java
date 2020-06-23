d8ffa3b4f6f43d556ff37f20e6d9d292d7e2242c
julija-sk <j.skopeca@gmail.com>
2020-06-23 19:00:11 +0300

    lesson 8 level 2 all tasks

---
+++ src/main/java/student_julija_skopeca/practical_tasks/lesson_8/level_2/task_9/ComputerPlayer.java
@@ -0,0 +1,8 @@
+package student_julija_skopeca.practical_tasks.lesson_8.level_2.task_9;
+
+class ComputerPlayer extends Player{
+
+    public ComputerPlayer (String name){
+        super(name);
+    }
+}
