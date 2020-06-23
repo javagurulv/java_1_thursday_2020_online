3447e83eae458ae26f4986d744ba8e881c90b35c
AlexBogachenkov <aleksbogac@gmail.com>
2020-06-23 16:31:59 +0300

    lesson 9, level 5, task 27 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson9_18062020/homeworks/level_5/task_27/FraudRule4.java
@@ -0,0 +1,13 @@
+package student_alexander_bogachenkov.lesson9_18062020.homeworks.level_5.task_27;
+
+class FraudRule4 extends FraudRule {
+
+    public FraudRule4(String ruleName) {
+        super(ruleName);
+    }
+
+    public boolean isFraud(Transaction t) {
+        return t.getTraderCountry().equals("Jamaica");
+    }
+
+}
