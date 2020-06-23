2fc14b4b2660012a825062546ce8f32fd17e92bb
julija-sk <j.skopeca@gmail.com>
2020-06-23 18:11:50 +0300

    lesson 8 level 1 task 3

---
+++ src/main/java/student_julija_skopeca/practical_tasks/lesson_8/level_3/DefaultConstuctor.java
@@ -0,0 +1,9 @@
+package student_julija_skopeca.practical_tasks.lesson_8.level_3;
+
+class DefaultConstuctor {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.getInfoOFPerson();
+    }
+}
