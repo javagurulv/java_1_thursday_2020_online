acef88a0d396622eb7cb7198c6939b1b5163dba8
julija-sk <j.skopeca@gmail.com>
2020-06-24 21:06:23 +0300

    lesson 8 level 3 all tasks

---
+++ src/main/java/student_julija_skopeca/practical_tasks/lesson_8/level_3/task_12/LoanSystem.java
@@ -0,0 +1,8 @@
+package student_julija_skopeca.practical_tasks.lesson_8.level_3.task_12;
+
+abstract class LoanSystem {
+
+    abstract boolean canReceiveLoan(Human human);
+}
+
+abstract class Human{}
