6177e8430c7132a12fbe6959ecc51dc29187fc97
AlexBogachenkov <aleksbogac@gmail.com>
2020-06-24 16:45:38 +0300

    lesson 9, level 6, task 30 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson9_18062020/homeworks/level_6/task_30/FraudDetectionResult.java
@@ -0,0 +1,28 @@
+package student_alexander_bogachenkov.lesson9_18062020.homeworks.level_6.task_30;
+
+class FraudDetectionResult {
+
+    private boolean fraud;
+    private String ruleName;
+
+    public FraudDetectionResult(boolean fraud, String ruleName) {
+        this.fraud = fraud;
+        this.ruleName = ruleName;
+    }
+
+    public boolean isFraud() {
+        return fraud;
+    }
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public void setFraud(boolean fraud) {
+        this.fraud = fraud;
+    }
+
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+}
