3c3519a5aa1dafc6a335e57f5dbca28ff267bb93
julija-sk <j.skopeca@gmail.com>
2020-06-25 21:18:34 +0300

    lesson 8 level 4 task 17

---
+++ src/main/java/student_julija_skopeca/practical_tasks/lesson_8/level_4/task_17/Square.java
@@ -0,0 +1,21 @@
+package student_julija_skopeca.practical_tasks.lesson_8.level_4.task_17;
+
+class Square extends Shape {
+
+    private int edge;
+
+    Square(int edge){
+        super("Square");
+        this.edge = edge;
+    }
+
+    @Override
+    double calculateArea() {
+        return (edge*edge);
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return (edge*4);
+    }
+}
