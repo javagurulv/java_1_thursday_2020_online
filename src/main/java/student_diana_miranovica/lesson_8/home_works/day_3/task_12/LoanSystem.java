27071eef384d723beb6c457a1cf2e3c2d28c6df2
Diana <63928714+Ciklop-Ciklop@users.noreply.github.com>
2020-06-29 22:00:25 +0300

    home work

---
+++ src/main/java/student_diana_miranovica/lesson_8/home_works/day_3/task_12/LoanSystem.java
@@ -0,0 +1,10 @@
+package student_diana_miranovica.lesson_8.home_works.day_3.task_12;
+
+ abstract class LoanSystem {
+
+      abstract boolean canReceiveLoan(Human human);
+}
+
+ abstract class Human{
+
+}
