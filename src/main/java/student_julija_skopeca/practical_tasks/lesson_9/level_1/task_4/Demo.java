c0df80266d5473b7bb0e4d28d5cf32e4f11f8b9c
julija-sk <j.skopeca@gmail.com>
2020-07-03 19:39:58 +0300

    lesson 9 level 1 tasks 4

---
+++ src/main/java/student_julija_skopeca/practical_tasks/lesson_9/level_1/task_4/Demo.java
@@ -0,0 +1,10 @@
+package student_julija_skopeca.practical_tasks.lesson_9.level_1.task_4;
+
+class Demo {
+    public static void main(String[] args) {
+        Safe safe = new Safe(1111,22);
+        safe.getPinCode();
+        safe.getSafeBalance();
+
+    }
+}
