7515a230cfa6f95a07a785497852b1d3636d2c71
julija-sk <j.skopeca@gmail.com>
2020-07-08 20:30:12 +0300

    lesson 9 level 2 tasks 16

---
+++ src/main/java/student_julija_skopeca/practical_tasks/lesson_9/level_3/task_16/SuperClassConstructorInvoker.java
@@ -0,0 +1,8 @@
+package student_julija_skopeca.practical_tasks.lesson_9.level_3.task_16;
+
+public class SuperClassConstructorInvoker {
+    public SuperClassConstructorInvoker() {
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
