eb52ad51f762471109717185e2ccaf4c9822da0c
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-07-08 19:07:15 +0300

    lesson_9 task_9

--- src/main/java/student_igors_mihejevs/lesson_9/level_2/PrivateConstructorDemo.java
+++ src/main/java/student_igors_mihejevs/lesson_9/level_2/PrivateConstructorDemo.java
@@ -2,6 +2,8 @@ package student_igors_mihejevs.lesson_9.level_2;
 
 class PrivateConstructorDemo {
 
-    // PrivateConstructor privateConstructor = new PrivateConstructor();
+    public static void main(String[] args) {
+        // PrivateConstructor privateConstructor = new PrivateConstructor();
+    }
 
 }
