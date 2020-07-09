10e6f0302f6691c8d5c11b007add676b9424a062
AlexBogachenkov <aleksbogac@gmail.com>
2020-07-09 16:39:29 +0300

    lesson 10, level 1, tasks added

---
+++ src/main/java/student_alexander_bogachenkov/lesson10_02072020/homeworks/level_1/task_2/ArrayUtil.java
@@ -0,0 +1,14 @@
+package student_alexander_bogachenkov.lesson10_02072020.homeworks.level_1.task_2;
+
+interface ArrayUtil {
+
+    public int[] createArray(int arrayLength);
+
+    public void fillArrayWithRandomNumbers(int[] array);
+
+    public void printArrayToConsole(int[] array);
+
+    public int findMaxNumber(int[] array);
+
+    public int findMinNumber(int[] array);
+}
---
+++ src/main/java/student_alexander_bogachenkov/lesson10_02072020/homeworks/level_1/task_3/MyList.java
@@ -0,0 +1,10 @@
+package student_alexander_bogachenkov.lesson10_02072020.homeworks.level_1.task_3;
+
+interface MyList {
+
+    //Добавление строки в список строк.
+    public void add(String stringToAdd);
+
+    //Удаление строки из списка строк.
+    public void remove(String stringToRemove);
+}
