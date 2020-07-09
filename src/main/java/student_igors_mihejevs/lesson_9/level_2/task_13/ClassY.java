4f700a9e7ae8fe47fa4f066758703ffeb4e3cf8f
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-07-09 17:10:07 +0300

    lesson_9 task_13

---
+++ src/main/java/student_igors_mihejevs/lesson_9/level_2/task_13/ClassY.java
@@ -0,0 +1,10 @@
+package student_igors_mihejevs.lesson_9.level_2.task_13;
+
+class ClassY extends ClassX{
+
+    public ClassY(int i) {
+        super(i);
+        System.out.println(2);
+    }
+
+}
