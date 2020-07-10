bf2753e16324aafc5eaf35cad66ae73a9f005113
AlexBogachenkov <aleksbogac@gmail.com>
2020-07-10 23:24:17 +0300

    lesson 10, level 6, task 16 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson10_02072020/homeworks/level_6/task_16/BookReader.java
@@ -0,0 +1,9 @@
+package student_alexander_bogachenkov.lesson10_02072020.homeworks.level_6.task_16;
+
+interface BookReader {
+
+    boolean addBook(Book book);
+
+    boolean deleteBook(Book book);
+
+}
