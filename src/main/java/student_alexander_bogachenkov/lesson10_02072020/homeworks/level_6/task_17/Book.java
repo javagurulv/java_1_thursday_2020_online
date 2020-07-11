77060f27a35c9d6b08ff18bb41531ec1fbd469b2
AlexBogachenkov <aleksbogac@gmail.com>
2020-07-11 13:27:53 +0300

    lesson 10, level 6, task 17 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson10_02072020/homeworks/level_6/task_17/Book.java
@@ -0,0 +1,36 @@
+package student_alexander_bogachenkov.lesson10_02072020.homeworks.level_6.task_17;
+
+import java.util.Objects;
+
+class Book {
+
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book that = (Book) o;
+        return title == that.title &&
+                Objects.equals(author, that.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
---
+++ src/main/java/student_alexander_bogachenkov/lesson10_02072020/homeworks/level_6/task_17/BookReader.java
@@ -0,0 +1,11 @@
+package student_alexander_bogachenkov.lesson10_02072020.homeworks.level_6.task_17;
+
+interface BookReader {
+
+    boolean addBook(Book book);
+
+    boolean deleteBook(Book book);
+
+    String[] getAllBooks();
+
+}
