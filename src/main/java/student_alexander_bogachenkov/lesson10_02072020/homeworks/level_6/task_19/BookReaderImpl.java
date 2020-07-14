ad72bfdb2e7313f513dc3740b9a3c1b526069376
AlexBogachenkov <aleksbogac@gmail.com>
2020-07-14 15:29:12 +0300

    lesson 10, level 6, task 19 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson10_02072020/homeworks/level_6/task_19/BookReaderImpl.java
@@ -0,0 +1,82 @@
+package student_alexander_bogachenkov.lesson10_02072020.homeworks.level_6.task_19;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class BookReaderImpl implements BookReader {
+
+    List<Book> books = new ArrayList<>();
+
+    @Override
+    public boolean addBook(Book book) {
+        if ((!hasTheSameBook(book)) && (indicatedTitle(book)) && (indicatedAuthor(book))) {
+            books.add(book);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deleteBook(Book book) {
+        if (hasTheSameBook(book)) {
+            books.remove(book);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String[] getAllBooks() {
+        String[] allBooks = new String[books.size()];
+        for (int i = 0; i < books.size(); i++) {
+            allBooks[i] = books.get(i).getTitle() + " [" + books.get(i).getAuthor() + "] ";
+        }
+        return allBooks;
+    }
+
+    @Override
+    public List<Book> findBooksByAuthor(String author) {
+        List<Book> booksWithSameAuthors = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getAuthor().equals(author) || Arrays.equals(getFirstFourLetters(author), book.getFirstFourAuthorLetters())) {
+                booksWithSameAuthors.add(book);
+            }
+        }
+        return booksWithSameAuthors;
+    }
+
+    char[] getFirstFourLetters(String author) {
+        String clearString = author.replaceAll("[^a-zA-Zа-яА-Я]", "");
+        String lowercased = clearString.toLowerCase();
+        char[] tempArray = lowercased.toCharArray();
+        char[] result = new char[4];
+        for (int i = 0; i < 4; i++) {
+            result[i] = tempArray[i];
+        }
+        return result;
+    }
+
+    private boolean hasTheSameBook(Book book) {
+        for (Book value : books) {
+            if (value.equals(book)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean indicatedTitle(Book book) {
+        return book.getTitle() != null && !book.getTitle().equals("");
+    }
+
+    private boolean indicatedAuthor(Book book) {
+        return book.getAuthor() != null && !book.getAuthor().equals("");
+    }
+
+    List<Book> getBooks() {
+        return books;
+    }
+}
