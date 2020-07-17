4ea76305407e92a8f2eeced1c60bf61e4ad03698
AlexBogachenkov <aleksbogac@gmail.com>
2020-07-17 17:57:03 +0300

    lesson 11, level 2, task 13 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson11_09072020/homeworks/level_2/task_13/BookDatabase.java
@@ -0,0 +1,24 @@
+package student_alexander_bogachenkov.lesson11_09072020.homeworks.level_2.task_13;
+
+import java.util.List;
+import java.util.Optional;
+
+interface BookDatabase {
+
+    Long save(Book book);
+
+    boolean delete(Long bookId);
+
+    boolean delete(Book book);
+
+    Optional<Book> findById(Long bookId);
+
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByTitle(String title);
+
+    int countAllBooks();
+
+    void deleteByAuthor(String author);
+
+}
---
+++ src/main/java/student_alexander_bogachenkov/lesson11_09072020/homeworks/level_2/task_13/BookDatabaseImpl.java
@@ -0,0 +1,115 @@
+package student_alexander_bogachenkov.lesson11_09072020.homeworks.level_2.task_13;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+class BookDatabaseImpl implements BookDatabase {
+
+    private final List<Book> books = new ArrayList<>();
+    private long lastId = 0;
+
+    @Override
+    public Long save(Book book) {
+        long id = nextId();
+        book.setId(id);
+        this.books.add(book);
+        return id;
+    }
+
+    @Override
+    public boolean delete(Long bookId) {
+        if (hasTheSameId(bookId)) {
+            for (Book book : books) {
+                if (book.getId().equals(bookId)) {
+                    books.remove(book);
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(Book book) {
+        if (hasTheSameBook(book)) {
+            books.remove(book);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Optional<Book> findById(Long bookId) {
+        if (hasTheSameId(bookId)) {
+            for (Book book : books) {
+                if (book.getId().equals(bookId)) {
+                    return Optional.of(book);
+                }
+            }
+            return Optional.empty();
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Book> findByAuthor(String author) {
+        List<Book> allBooks = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getAuthor().equals(author)) {
+                allBooks.add(book);
+            }
+        }
+        return allBooks;
+    }
+
+    @Override
+    public List<Book> findByTitle(String title) {
+        List<Book> allBooks = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getTitle().equals(title)) {
+                allBooks.add(book);
+            }
+        }
+        return allBooks;
+    }
+
+    @Override
+    public int countAllBooks() {
+        return books.size();
+    }
+
+    @Override
+    public void deleteByAuthor(String author) {
+        books.removeIf(book -> book.getAuthor().equals(author));
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    private Long nextId() {
+        return ++lastId;
+    }
+
+    private boolean hasTheSameId(Long bookId) {
+        for (Book book : books) {
+            if (book.getId().equals(bookId)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasTheSameBook(Book book) {
+        for (Book value : books) {
+            if (value.equals(book)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
