2538b5eae2583406a273778c809193ee061e5699
AlexBogachenkov <aleksbogac@gmail.com>
2020-07-17 18:28:39 +0300

    lesson 11, level 2, task 14 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson11_09072020/homeworks/level_2/task_14/BookDatabase.java
@@ -0,0 +1,26 @@
+package student_alexander_bogachenkov.lesson11_09072020.homeworks.level_2.task_14;
+
+import java.util.List;
+import java.util.Optional;
+
+interface BookDatabase {
+
+    Long save(Book book);
+
+    boolean delete(Long bookId);
+
+    boolean delete(Book book);
+
+    Optional<Book> findById(Long bookId);
+
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByTitle(String title);
+
+    int countAllBooks();
+
+    void deleteByAuthor(String author);
+
+    void deleteByTitle(String title);
+
+}
