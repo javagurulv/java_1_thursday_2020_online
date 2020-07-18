e123f070a4d77b02d7ff576b41705f96786ef680
AlexBogachenkov <aleksbogac@gmail.com>
2020-07-18 15:49:25 +0300

    lesson 11, level 3, task 22 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson11_09072020/homeworks/level_3/task_22/SearchCriteriaDemo.java
@@ -0,0 +1,11 @@
+package student_alexander_bogachenkov.lesson11_09072020.homeworks.level_3.task_22;
+
+class SearchCriteriaDemo {
+
+    public static void main(String[] args) {
+        SearchCriteria authorSearchCriteria = new AuthorSearchCriteria("Kuper");
+        SearchCriteria titleSearchCriteria = new TitleSearchCriteria("Zveroboi");
+        SearchCriteria searchCriteria = new AndSearchCriteria(authorSearchCriteria, titleSearchCriteria);
+    }
+
+}
