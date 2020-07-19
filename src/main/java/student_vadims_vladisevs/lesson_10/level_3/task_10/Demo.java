974bf51384241dac8e6f0efea596d32776557729
VadimsVladisevs <vadims.vladisevs@gmail.com>
2020-07-19 17:00:18 +0300

    error correction

--- src/main/java/student_vadims_vladisevs/lesson_10/level_3/task_10/Demo.java
+++ src/main/java/student_vadims_vladisevs/lesson_10/level_3/task_10/Demo.java
@@ -1,6 +1,8 @@
 package student_vadims_vladisevs.lesson_10.level_3.task_10;
 
- class Demo {
+import java.util.Optional;
+
+class Demo {
      public static void main(String[] args) {
          InMemoryDatabase inMemoryDatabase = new InMemoryDatabase();
 
@@ -12,14 +14,14 @@ package student_vadims_vladisevs.lesson_10.level_3.task_10;
          inMemoryDatabase.save(product2);
          inMemoryDatabase.save(product3);
 
-         Product result1 = inMemoryDatabase.findByTitle("First Product");
-         System.out.println(result1.getTitle());
+         Optional<Product> result1 = inMemoryDatabase.findByTitle("First Product");
+         System.out.println(result1.get().getTitle());
 
-         Product result2 = inMemoryDatabase.findByTitle("Second Product");
-         System.out.println(result2.getTitle());
+         Optional<Product> result2 = inMemoryDatabase.findByTitle("Second Product");
+         System.out.println(result2.get().getTitle());
 
-         Product result3 = inMemoryDatabase.findByTitle("Third Product");
-         System.out.println(result3.getTitle());
+         Optional<Product> result3 = inMemoryDatabase.findByTitle("Third Product");
+         System.out.println(result3.get().getTitle());
 
      }
 }
