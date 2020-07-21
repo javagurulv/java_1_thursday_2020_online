92a447ab6a790c9d08d3a403b42e4e8bd53d54b5
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-07-21 14:52:49 +0300

    Lesson 11 lvl 2 task 10

---
+++ src/main/java/student_dmitrijs_visuns/homeworks/lesson_11/level_2/task_10/BookDataBaseImpl.java
@@ -0,0 +1,86 @@
+package student_dmitrijs_visuns.homeworks.lesson_11.level_2.task_10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+class BookDataBaseImpl  implements BookDataBase {
+
+    List<Book> booksList = new ArrayList<>();
+    private Long bookId = 1L;
+
+    @Override
+    public Long save(Book book) {
+        book.setId(bookId);
+        booksList.add(book);
+        bookId++;
+        return bookId;
+    }
+
+    @Override
+    public boolean delete(Long bookId) {
+        if (!isBookIdInTheBooksList(bookId)) {
+            return false;
+        } else {
+            booksList.remove(getBookIndexInListById(bookId));
+            return true;
+        }
+    }
+
+
+    @Override
+    public boolean delete (Book book) {
+        if (!isBookInBooksList(book)) {
+            return false;
+        } else {
+            booksList.remove(book);
+            return true;
+        }
+    }
+
+    @Override
+    public List<Book> findByAuthor(String author) {
+        List<Book> booksByAuthor = new ArrayList<>();
+        for (Book book : booksList) {
+            if (book.getAuthor().equals(author)) {
+                booksByAuthor.add(book);
+            }
+        } return booksByAuthor;
+    }
+
+    @Override
+    public Optional<Book> findById(Long bookId) {
+        for (Book book : booksList) {
+            if (book.getId().equals(bookId)) {
+                return Optional.of(book);
+            }
+        } return Optional.empty();
+    }
+
+    private boolean isBookInBooksList (Book book) {
+        for (Book bookInList : booksList) {
+            if (bookInList.equals(book)) {
+                return true;
+            }
+        } return false;
+    }
+
+    private int getBookIndexInListById (Long bookId) {
+        int bookIndexInList = 0;
+        for (int i = 0; i < booksList.size(); i++) {
+            if (booksList.get(i).getId().equals(bookId)) {
+                bookIndexInList = i;
+            }
+        } return bookIndexInList;
+    }
+
+    private boolean isBookIdInTheBooksList(Long bookId) {
+        for (Book book : booksList) {
+            if (book.getId().equals(bookId)) {
+                return true;
+            }
+        } return false;
+    }
+
+
+}
