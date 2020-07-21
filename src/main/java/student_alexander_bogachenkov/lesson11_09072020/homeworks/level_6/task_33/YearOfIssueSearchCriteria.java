5fcea5c5b9fec73c76794f3aa9fd1089bb63c1a7
AlexBogachenkov <aleksbogac@gmail.com>
2020-07-21 20:12:38 +0300

    lesson 11, level 6, task 33 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson11_09072020/homeworks/level_6/task_33/YearOfIssueSearchCriteria.java
@@ -0,0 +1,15 @@
+package student_alexander_bogachenkov.lesson11_09072020.homeworks.level_6.task_33;
+
+class YearOfIssueSearchCriteria implements SearchCriteria {
+
+    private String yearOfIssueToSearch;
+
+    public YearOfIssueSearchCriteria(String yearOfIssueToSearch) {
+        this.yearOfIssueToSearch = yearOfIssueToSearch;
+    }
+
+    public boolean match(Book book) {
+        return book.getYearOfIssue().equals(yearOfIssueToSearch);
+    }
+
+}
