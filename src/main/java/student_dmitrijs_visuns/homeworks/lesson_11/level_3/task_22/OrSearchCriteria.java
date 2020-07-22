fb956005335a7fdac9e8380ee9195adc51860e85
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-07-22 15:24:13 +0300

    Lesson 11 lvl 3 tasks done

---
+++ src/main/java/student_dmitrijs_visuns/homeworks/lesson_11/level_3/task_22/OrSearchCriteria.java
@@ -0,0 +1,19 @@
+package student_dmitrijs_visuns.homeworks.lesson_11.level_3.task_22;
+
+class OrSearchCriteria implements SearchCriteria {
+
+    private SearchCriteria leftCondition;
+    private SearchCriteria rightCondition;
+
+    public OrSearchCriteria(SearchCriteria leftCondition,
+                            SearchCriteria rightCondition) {
+        this.leftCondition = leftCondition;
+        this.rightCondition = rightCondition;
+    }
+
+    @Override
+    public boolean match(Book book) {
+        return this.leftCondition.match(book) || this.rightCondition.match(book);
+    }
+
+}
