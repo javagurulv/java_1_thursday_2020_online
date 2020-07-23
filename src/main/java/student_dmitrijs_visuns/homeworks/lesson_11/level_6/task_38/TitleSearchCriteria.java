f39fd69abdfef1775788f5dbc939caea40e876f8
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-07-23 11:52:27 +0300

    Lesson 11 tasks done

---
+++ src/main/java/student_dmitrijs_visuns/homeworks/lesson_11/level_6/task_38/TitleSearchCriteria.java
@@ -0,0 +1,16 @@
+package student_dmitrijs_visuns.homeworks.lesson_11.level_6.task_38;
+
+class TitleSearchCriteria implements SearchCriteria {
+
+    private String titleToSearch;
+
+    public TitleSearchCriteria(String titleToSearch) {
+        this.titleToSearch = titleToSearch;
+    }
+
+    @Override
+    public boolean match(Book book) {
+        return book.getTitle().equals(this.titleToSearch);
+    }
+
+}
