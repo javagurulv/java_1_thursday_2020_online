036730f9190fb9d92ccc82ceb14a79047dcfee0a
AlexBogachenkov <aleksbogac@gmail.com>
2020-07-23 17:20:36 +0300

    lesson 12, level 2, task 10 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson12_16072020/homeworks/level_2/task_10/Role.java
@@ -0,0 +1,7 @@
+package student_alexander_bogachenkov.lesson12_16072020.homeworks.level_2.task_10;
+
+enum Role {
+
+    CAN_SEARCH_CLIENTS
+
+}
---
+++ src/main/java/student_alexander_bogachenkov/lesson12_16072020/homeworks/level_2/task_10/UserCredentials.java
@@ -0,0 +1,13 @@
+package student_alexander_bogachenkov.lesson12_16072020.homeworks.level_2.task_10;
+
+import java.util.List;
+
+class UserCredentials {
+
+    private List<Role> roles;
+
+    public UserCredentials(List<Role> roles) {
+        this.roles = roles;
+    }
+
+}
