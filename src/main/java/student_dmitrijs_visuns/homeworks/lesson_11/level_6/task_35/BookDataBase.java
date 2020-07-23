ae68c6de81653f00e7275670e0efc359699d4ece
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-07-23 11:22:11 +0300

    Lesson 11 lvl 6 task 35

---
+++ src/main/java/student_dmitrijs_visuns/homeworks/lesson_11/level_6/task_35/BookDataBase.java
@@ -0,0 +1,44 @@
+package student_dmitrijs_visuns.homeworks.lesson_11.level_6.task_35;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+interface BookDataBase {
+
+    Long save (Book book);
+
+    boolean delete(Long bookId);
+
+    boolean delete(Book book);
+
+    Optional<Book> findById(Long bookId);
+
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByTitle(String title);
+
+    int countAllBooks();
+
+    void deleteByAuthor(String author);
+
+    void deleteByTitle(String title);
+
+    List<Book> find(SearchCriteria searchCriteria);
+
+    Set<String> findUniqueAuthors();
+
+    Set<String> findUniqueTitles();
+
+    Set<Book> findUniqueBooks();
+
+    boolean contains(Book book);
+
+    Map<String, List> getAuthorToBooksMap();
+
+    Map<String, Integer> getEachAuthorBookCount();
+
+
+
+}
