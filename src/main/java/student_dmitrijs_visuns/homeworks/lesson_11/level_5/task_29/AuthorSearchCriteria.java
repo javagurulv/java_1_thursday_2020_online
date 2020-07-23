e21260cac28624c2765f3270ca123c76522ede17
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-07-23 08:26:50 +0300

    Lesson 11 lvl 5 tasks done

---
+++ src/main/java/student_dmitrijs_visuns/homeworks/lesson_11/level_5/task_29/AuthorSearchCriteria.java
@@ -0,0 +1,15 @@
+package student_dmitrijs_visuns.homeworks.lesson_11.level_5.task_29;
+
+class AuthorSearchCriteria implements SearchCriteria {
+
+    private String authorToSearch;
+
+    public AuthorSearchCriteria(String authorToSearch) {
+        this.authorToSearch = authorToSearch;
+    }
+
+    public boolean match(Book book) {
+        return book.getAuthor().equals(this.authorToSearch);
+    }
+
+}
---
+++ src/main/java/student_dmitrijs_visuns/homeworks/lesson_11/level_5/task_29/OrSearchCriteria.java
@@ -0,0 +1,19 @@
+package student_dmitrijs_visuns.homeworks.lesson_11.level_5.task_29;
+
+class OrSearchCriteria implements SearchCriteria {
+
+    private SearchCriteria leftCondition;
+    private SearchCriteria rightCondition;
+
+    public OrSearchCriteria(SearchCriteria leftCondition,
+                            SearchCriteria rightCondition) {
+        this.leftCondition = leftCondition;
+        this.rightCondition = rightCondition;
+    }
+
+    @Override
+    public boolean match(Book book) {
+        return this.leftCondition.match(book) || this.rightCondition.match(book);
+    }
+
+}
