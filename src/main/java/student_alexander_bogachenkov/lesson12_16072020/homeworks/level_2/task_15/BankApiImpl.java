cc4149e56b6b5ffe8c40cd00180cda0ff58075ff
AlexBogachenkov <aleksbogac@gmail.com>
2020-07-25 13:04:21 +0300

    lesson 12, level 2, task 15 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson12_16072020/homeworks/level_2/task_15/BankApiImpl.java
@@ -0,0 +1,31 @@
+package student_alexander_bogachenkov.lesson12_16072020.homeworks.level_2.task_15;
+
+import java.util.List;
+import java.util.Optional;
+
+class BankApiImpl implements BankApi {
+
+    private List<BankClient> clients;
+
+    public BankApiImpl(List<BankClient> clients) {
+        this.clients = clients;
+    }
+
+    public Optional<BankClient> findByUid(UserCredentials credentials,
+                                          String uid) throws AccessDeniedException {
+        // если в credentials нет роли Role.CAN_SEARCH_CLIENTS
+        // то метод должен кидать ошибку AccessDeniedException
+        // иначе производить поиск клиента с указанным uid
+        boolean credentialsContainsCAN_SEARCH_CLIENTS = credentials.getRoles().contains(Role.CAN_SEARCH_CLIENTS);
+        if (!(credentialsContainsCAN_SEARCH_CLIENTS)) {
+            throw new AccessDeniedException();
+        } else {
+            for (BankClient client : clients) {
+                if (client.getUid().equals(uid)) {
+                    return Optional.of(client);
+                }
+            }
+            return Optional.empty();
+        }
+    }
+}
