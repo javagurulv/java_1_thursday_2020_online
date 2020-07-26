4d2cbc51724cfc1d12ffd7edd8cc99fce45dfe18
AlexBogachenkov <aleksbogac@gmail.com>
2020-07-26 14:33:50 +0300

    lesson 12, level 5, task 34 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson12_16072020/homeworks/level_5/task_34/Product.java
@@ -0,0 +1,65 @@
+package student_alexander_bogachenkov.lesson12_16072020.homeworks.level_5.task_34;
+
+import java.util.Objects;
+
+class Product {
+
+    private String title;
+    private Integer price;
+    private String description;
+
+    Product(String title, Integer price, String description) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product that = (Product) o;
+        return title == that.title &&
+                Objects.equals(price, that.price) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, price, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "title='" + title + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+}
