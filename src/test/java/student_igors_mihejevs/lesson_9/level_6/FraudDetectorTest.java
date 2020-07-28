1b7b8f505cb11db912705652b6af13509c16d857
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-07-28 22:18:32 +0300

    lesson_10 task_1

--- src/test/java/student_igors_mihejevs/lesson_9/level_6/FraudDetectorTest.java
+++ src/test/java/student_igors_mihejevs/lesson_9/level_6/FraudDetectorTest.java
@@ -2,9 +2,11 @@ package student_igors_mihejevs.lesson_9.level_6;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class FraudDetectorTest {
+
 
     @Test
     public void fraudDetectorTestPositiveFraudRule1() {
