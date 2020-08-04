fe251c63507db5ec9f715ff0e0647c7d0e6d41c7
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-08-04 20:11:41 +0300

    lesson_10 task_10 refactoring

---
+++ src/main/java/student_igors_mihejevs/lesson_10/homework/level_3/task_10/Database.java
@@ -0,0 +1,11 @@
+package student_igors_mihejevs.lesson_10.homework.level_3.task_10;
+
+import java.util.Optional;
+
+interface Database {
+
+    void save(Product product);
+
+    Optional<Product> findByTitle(String productTitle);
+
+}
