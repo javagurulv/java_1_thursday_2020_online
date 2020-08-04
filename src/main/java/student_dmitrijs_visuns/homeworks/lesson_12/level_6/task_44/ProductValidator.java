9b42a7c30e8f797173ddfbc32e26a35f5ca22024
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-08-04 21:56:39 +0300

    Lesson 12 level 6 tasks in progress

---
+++ src/main/java/student_dmitrijs_visuns/homeworks/lesson_12/level_6/task_44/ProductValidator.java
@@ -0,0 +1,9 @@
+package student_dmitrijs_visuns.homeworks.lesson_12.level_6.task_44;
+
+import java.util.List;
+
+interface ProductValidator {
+
+    List<ValidationException> validate (Product product);
+
+}
