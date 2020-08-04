a81a70dc48dcfc050ecb88c0b9bbd8f36cbdfcdc
AlexBogachenkov <aleksbogac@gmail.com>
2020-08-04 13:06:38 +0300

    lesson 14, level 4, task 31 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson14_30072020/homeworks/level_4/task_31/Transaction.java
@@ -0,0 +1,40 @@
+package student_alexander_bogachenkov.lesson14_30072020.homeworks.level_4.task_31;
+
+class Transaction {
+
+    private Trader trader;
+    private int year;
+    private int value;
+
+    public Transaction(Trader trader, int year, int value) {
+        this.trader = trader;
+        this.year = year;
+        this.value = value;
+    }
+
+    public Trader getTrader() {
+        return this.trader;
+    }
+
+    public String getTraderName() {
+        return trader.getName();
+    }
+
+    public String getTraderCity() {
+        return trader.getCity();
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public String toString() {
+        return "{" + this.trader + ", " +
+                "year: " + this.year + ", " +
+                "value:" + this.value + "}";
+    }
+}
