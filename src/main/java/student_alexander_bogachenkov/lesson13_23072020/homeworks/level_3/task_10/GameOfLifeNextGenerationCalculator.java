04fe957b173aac5eaca70fd35301b09c7f851a3e
AlexBogachenkov <aleksbogac@gmail.com>
2020-08-05 13:22:33 +0300

    lesson 13, level 3, task 10 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson13_23072020/homeworks/level_3/task_10/GameOfLifeNextGenerationCalculator.java
@@ -0,0 +1,9 @@
+package student_alexander_bogachenkov.lesson13_23072020.homeworks.level_3.task_10;
+
+class GameOfLifeNextGenerationCalculator {
+
+    public boolean[][] calculate(boolean[][] currentGeneration) {
+        return null;
+    }
+
+}
