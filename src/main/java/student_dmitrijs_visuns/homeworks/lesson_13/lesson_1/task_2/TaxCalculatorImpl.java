9783e0d9bf577b12fae152c50a3eff1a6ebebb6b
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-08-05 10:35:00 +0300

    Lesson 13 tasks in progress

---
+++ src/main/java/student_dmitrijs_visuns/homeworks/lesson_13/lesson_1/task_2/TaxCalculatorImpl.java
@@ -0,0 +1,13 @@
+package student_dmitrijs_visuns.homeworks.lesson_13.lesson_1.task_2;
+
+import java.math.BigDecimal;
+
+public class TaxCalculatorImpl implements TaxCalculator {
+
+    public BigDecimal calculateTax(BigDecimal income) {
+        return null;
+    }
+
+}
+
+
