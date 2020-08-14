a851fecf8a6376c647c3984a2c11451ebec059c1
AlexBogachenkov <aleksbogac@gmail.com>
2020-08-14 18:58:36 +0300

    lesson x, bank account, task 10 added

--- src/main/java/student_alexander_bogachenkov/lesson_x/bank_account/part_1/BankAccountMaxBalanceFinder.java
+++ src/main/java/student_alexander_bogachenkov/lesson_x/bank_account/part_1/BankAccountMaxBalanceFinder.java
@@ -4,9 +4,15 @@ class BankAccountMaxBalanceFinder {
 
     public int find(BankAccount bankAccount) {
         int maxBalance = 0;
+        int currentBalance = 0;
         for (Transaction trn : bankAccount.getTransactions()) {
-            if (trn.getAmount() > maxBalance) {
-                maxBalance = trn.getAmount();
+            if (trn.isDeposit()) {
+                currentBalance += trn.getAmount();
+            } else if (trn.isWithdrawal()) {
+                currentBalance -= trn.getAmount();
+            }
+            if (currentBalance > maxBalance) {
+                maxBalance = currentBalance;
             }
         }
         return maxBalance;
