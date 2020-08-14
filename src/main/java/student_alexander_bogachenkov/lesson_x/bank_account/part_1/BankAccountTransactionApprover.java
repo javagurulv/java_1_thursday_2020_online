95958059edfc67adb66cf386681eb5663b5350ed
AlexBogachenkov <aleksbogac@gmail.com>
2020-08-14 19:36:33 +0300

    lesson x, bank account, task 11 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson_x/bank_account/part_1/BankAccountTransactionApprover.java
@@ -0,0 +1,13 @@
+package student_alexander_bogachenkov.lesson_x.bank_account.part_1;
+
+class BankAccountTransactionApprover {
+
+    private BankAccountCurrentBalanceCalculator currentBalanceCalculator;
+
+    public BankAccountTransactionApprover(BankAccountCurrentBalanceCalculator currentBalanceCalculator) {
+        this.currentBalanceCalculator = currentBalanceCalculator;
+    }
+
+
+
+}
