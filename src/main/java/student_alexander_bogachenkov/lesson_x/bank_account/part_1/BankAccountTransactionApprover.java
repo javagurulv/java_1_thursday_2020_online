e9795096d9f8b882a08f19d8b81d7ec38129b11f
AlexBogachenkov <aleksbogac@gmail.com>
2020-08-14 19:47:17 +0300

    lesson x, bank account, task 12 added

--- src/main/java/student_alexander_bogachenkov/lesson_x/bank_account/part_1/BankAccountTransactionApprover.java
+++ src/main/java/student_alexander_bogachenkov/lesson_x/bank_account/part_1/BankAccountTransactionApprover.java
@@ -8,6 +8,13 @@ class BankAccountTransactionApprover {
         this.currentBalanceCalculator = currentBalanceCalculator;
     }
 
-
+    public boolean approve(BankAccount bankAccount, Transaction newTransaction) {
+        int currentBalance = currentBalanceCalculator.calculate(bankAccount);
+        if (newTransaction.isDeposit()) {
+            return true;
+        } else {
+            return newTransaction.isWithdrawal() && currentBalance >= newTransaction.getAmount();
+        }
+    }
 
 }
