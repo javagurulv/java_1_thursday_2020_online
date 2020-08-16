b10dc104702523fd07d44c9f0abfd4c8aa2a19da
AlexBogachenkov <aleksbogac@gmail.com>
2020-08-16 12:48:12 +0300

    lesson x, bank account, task 17 added

---
+++ src/main/java/student_alexander_bogachenkov/lesson_x/bank_account/part_2/ExpenseCategory.java
@@ -0,0 +1,12 @@
+package student_alexander_bogachenkov.lesson_x.bank_account.part_2;
+
+enum ExpenseCategory {
+
+    COMMUNAL_PAYMENTS,
+    FOOD,
+    ENTERTAINMENT,
+    GAMBLING,
+    LOANS,
+    OTHERS
+
+}
---
+++ src/main/java/student_alexander_bogachenkov/lesson_x/bank_account/part_2/TransactionType.java
@@ -0,0 +1,7 @@
+package student_alexander_bogachenkov.lesson_x.bank_account.part_2;
+
+enum TransactionType {
+
+    DEPOSIT, WITHDRAWAL
+
+}
