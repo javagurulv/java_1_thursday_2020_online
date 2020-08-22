afe26edfc964c78ab2c26f10c0143548d129c80b
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-08-22 09:55:41 +0300

    lesson_7 task_13 Copy with jUnit test

---
+++ src/main/java/student_igors_mihejevs/lesson_7/homework/level_6/Copy.java
@@ -0,0 +1,25 @@
+package student_igors_mihejevs.lesson_7.homework.level_6;
+
+public class Copy {
+
+    int[] copyInRange(int[] in, int numberFrom, int numberTo) {
+        int[] finalArray = new int[countingRangeOfFinalArray(in, numberFrom, numberTo)];
+        int finalArrayCount = 0;
+        for (int i : in) {
+            if ((i >= numberFrom) && (i <= numberTo)) {
+                finalArray[finalArrayCount] = i;
+                finalArrayCount++;
+            }
+        }
+        return finalArray;
+    }
+
+    private int countingRangeOfFinalArray(int[] in, int numberFrom, int numberTo) {
+        int count = 0;
+        for (int i : in) {
+            if ((i >= numberFrom) && (i <= numberTo)) count++;
+        }
+        return count;
+    }
+
+}
--- src/main/java/student_igors_mihejevs/lesson_7/homework/level_6/Palindrome.java
+++ src/main/java/student_igors_mihejevs/lesson_7/homework/level_6/Palindrome.java
@@ -2,7 +2,7 @@ package student_igors_mihejevs.lesson_7.homework.level_6;
 
 import java.util.Arrays;
 
-public class Palindrome {
+class Palindrome {
 
     boolean isPalindrome(String text) {
         String lowerCaseText = text.replaceAll("[^A-Za-zА-Яа-я0-9]", "").toLowerCase();
