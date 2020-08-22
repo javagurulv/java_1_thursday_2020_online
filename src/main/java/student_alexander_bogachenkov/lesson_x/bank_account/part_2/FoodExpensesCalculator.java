31427a41f4f667706a89981f0068482fdd898eae
AlexBogachenkov <aleksbogac@gmail.com>
2020-08-22 19:23:39 +0300

    lesson x, bank account, task 24 added

--- src/main/java/student_alexander_bogachenkov/lesson_x/bank_account/part_2/FoodExpensesCalculator.java
+++ src/main/java/student_alexander_bogachenkov/lesson_x/bank_account/part_2/FoodExpensesCalculator.java
@@ -1,34 +1,40 @@
 package student_alexander_bogachenkov.lesson_x.bank_account.part_2;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
 
 class FoodExpensesCalculator {
 
     public int calculateExpensesAmount(BankAccount bankAccount) {
         Transaction[] allTransactions = bankAccount.getTransactions();
-        Transaction[] withdrawalTransactions = getWithdrawalTransactions(allTransactions);
-        Transaction[] foodWithdrawalTransactions = getFoodWithdrawalTransactions(withdrawalTransactions);
+        List<Transaction> withdrawalTransactions = getWithdrawalTransactions(allTransactions);
+        List<Transaction> foodWithdrawalTransactions = getFoodWithdrawalTransactions(withdrawalTransactions);
         return getExpensesAmount(foodWithdrawalTransactions);
     }
 
-    private Transaction[] getWithdrawalTransactions(Transaction[] allTransactions) {
-        Stream<Transaction> transactionStream = Arrays.stream(allTransactions);
-        return transactionStream
-                .filter(transaction -> transaction.getTransactionType().equals(TransactionType.WITHDRAWAL))
-                .toArray(Transaction[]::new);
+    private List<Transaction> getWithdrawalTransactions(Transaction[] transactions) {
+        List<Transaction> result = new ArrayList<>();
+        for (Transaction transaction : transactions) {
+            if (transaction.isWithdrawal()) {
+                result.add(transaction);
+            }
+        }
+        return result;
     }
 
-    private Transaction[] getFoodWithdrawalTransactions(Transaction[] withdrawalTransactions) {
-        Stream<Transaction> transactionStream = Arrays.stream(withdrawalTransactions);
-        return transactionStream
-                .filter(transaction -> transaction.getExpenseCategory().equals(ExpenseCategory.FOOD))
-                .toArray(Transaction[]::new);
+    private List<Transaction> getFoodWithdrawalTransactions(List<Transaction> transactions) {
+        List<Transaction> result = new ArrayList<>();
+        for (Transaction transaction : transactions) {
+            if (transaction.getExpenseCategory() == ExpenseCategory.FOOD) {
+                result.add(transaction);
+            }
+        }
+        return result;
     }
 
-    private int getExpensesAmount(Transaction[] foodWithdrawalTransactions) {
+    private int getExpensesAmount(List<Transaction> transactions) {
         int result = 0;
-        for (Transaction transaction : foodWithdrawalTransactions) {
+        for (Transaction transaction : transactions) {
             result += transaction.getAmount();
         }
         return result;
