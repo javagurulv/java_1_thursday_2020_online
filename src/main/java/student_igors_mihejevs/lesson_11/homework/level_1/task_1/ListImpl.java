dd2f5810e45a75c7ce24f12c09b5223fff7d3fef
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-08-24 20:42:06 +0300

    lesson_11 task_1

---
+++ src/main/java/student_igors_mihejevs/lesson_11/homework/level_1/task_1/ListImpl.java
@@ -0,0 +1,24 @@
+package student_igors_mihejevs.lesson_11.homework.level_1.task_1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListImpl {
+
+    /* ArrayList and LinkedList.
+    Java ArrayList class uses a dynamic array for storing the elements.
+    It is like an array, but there is no size limit.
+    We can add or remove elements anytime. So, it is much more flexible than the traditional array.
+    It is found in the java.util package.
+
+    A linked list is a linear data structure, in which the elements are not stored at contiguous memory locations.
+    The elements in a linked list are linked using pointers.
+    In simple words, a linked list consists of nodes where each node contains a data field
+    and a reference(link) to the next node in the list.
+    */
+
+    List<Integer> numberAL = new ArrayList<>();
+    List<Integer> numberLL = new LinkedList<>();
+
+}
