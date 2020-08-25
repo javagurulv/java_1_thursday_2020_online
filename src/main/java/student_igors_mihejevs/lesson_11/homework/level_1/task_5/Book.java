0d54dbea5402d95848a46a1fa1a731f6b8deab27
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-08-25 16:24:54 +0300

    lesson_11 task_5

---
+++ src/main/java/student_igors_mihejevs/lesson_11/homework/level_1/task_5/Book.java
@@ -0,0 +1,35 @@
+package student_igors_mihejevs.lesson_11.homework.level_1.task_5;
+
+import java.util.Objects;
+
+class Book {
+    private final String title;
+    private final String author;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return title.equals(book.title) &&
+                author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author);
+    }
+}
