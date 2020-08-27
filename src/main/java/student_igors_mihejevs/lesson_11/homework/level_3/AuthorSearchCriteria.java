790478d2ee7499f82f11dff574ca2d6f82660216
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-08-27 11:55:10 +0300

    lesson_11 task_16 added

---
+++ src/main/java/student_igors_mihejevs/lesson_11/homework/level_3/AuthorSearchCriteria.java
@@ -0,0 +1,15 @@
+package student_igors_mihejevs.lesson_11.homework.level_3;
+
+class AuthorSearchCriteria implements SearchCriteria {
+
+    private final String authorToSearch;
+
+    public AuthorSearchCriteria(String authorToSearch) {
+        this.authorToSearch = authorToSearch;
+    }
+
+    public boolean match(Book book) {
+        return book.getAuthor().equals(this.authorToSearch);
+    }
+
+}
---
+++ src/test/java/student_igors_mihejevs/lesson_11/homework/level_3/AuthorSearchCriteriaTest.java
@@ -0,0 +1,19 @@
+package student_igors_mihejevs.lesson_11.homework.level_3;
+
+import org.junit.Test;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class AuthorSearchCriteriaTest {
+
+    @Test
+    public void shouldReturnTrueIfBookByAuthorIsCorrectlyFound() {
+        AuthorSearchCriteria authorSearchCriteria = new AuthorSearchCriteria("Joshua Bloch");
+        Book book1 = new Book("Kathy Sierra, Bert Bates", "Head First Java");
+        Book book2 = new Book("Joshua Bloch", "Effective Java");
+        assertFalse(authorSearchCriteria.match(book1));
+        assertTrue(authorSearchCriteria.match(book2));
+    }
+
+}
