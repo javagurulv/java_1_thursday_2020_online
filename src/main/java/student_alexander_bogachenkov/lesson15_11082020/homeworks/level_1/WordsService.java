cd5052b752aa378f0ff6a936ab5a3abd610bcac8
AlexBogachenkov <aleksbogac@gmail.com>
2020-09-02 18:09:44 +0300

    lesson 15, level 1, task 4 added

--- src/main/java/student_alexander_bogachenkov/lesson15_11082020/homeworks/level_1/WordsService.java
+++ src/main/java/student_alexander_bogachenkov/lesson15_11082020/homeworks/level_1/WordsService.java
@@ -1,4 +1,4 @@
-package student_alexander_bogachenkov.lesson15_11082020.homeworks.level_1.task_3;
+package student_alexander_bogachenkov.lesson15_11082020.homeworks.level_1;
 
 class WordsService {
 
