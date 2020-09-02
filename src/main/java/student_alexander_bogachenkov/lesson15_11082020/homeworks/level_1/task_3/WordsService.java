98f80d1d10825b1c36fd08ea33b7bc023794f74c
AlexBogachenkov <aleksbogac@gmail.com>
2020-09-02 18:09:03 +0300

    lesson 15, level 1, task 4 added

--- src/main/java/student_alexander_bogachenkov/lesson15_11082020/homeworks/level_1/task_3/WordsService.java
+++ src/main/java/student_alexander_bogachenkov/lesson15_11082020/homeworks/level_1/task_3/WordsService.java
@@ -2,14 +2,14 @@ package student_alexander_bogachenkov.lesson15_11082020.homeworks.level_1.task_3
 
 class WordsService {
 
-    public int countRepetitions(String[] words, String wordToCountForRepetition) {
-        int repetitionSum = 0;
+    public int countRepetitions(String[] words, String repeatingWord) {
+        int repetitions = 0;
         for (String word : words) {
-            if (word.equals(wordToCountForRepetition)) {
-                repetitionSum++;
+            if (word.equals(repeatingWord)) {
+                repetitions++;
             }
         }
-        return repetitionSum;
+        return repetitions;
     }
 
 }
