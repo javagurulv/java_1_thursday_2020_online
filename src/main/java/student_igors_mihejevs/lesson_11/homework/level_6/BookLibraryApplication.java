5dbf7a54d078a60cf90c61ad90696fa7108fd04e
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-09-03 21:12:27 +0300

    lesson_11 task_37 added

---
+++ src/main/java/student_igors_mihejevs/lesson_11/homework/level_6/BookLibraryApplication.java
@@ -0,0 +1,10 @@
+package student_igors_mihejevs.lesson_11.homework.level_6;
+
+class BookLibraryApplication {
+
+    public static void main(String[] args) {
+        BookLibrary bookLibrary = new BookLibrary();
+        bookLibrary.run();
+    }
+
+}
