ae2bdf465a0612fe796cd16a7940d182016e4737
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-09-03 15:31:02 +0300

    lesson_11 task_31 aded

---
+++ src/main/java/student_igors_mihejevs/lesson_11/homework/level_6/SaveBookUIAction.java
@@ -0,0 +1,26 @@
+package student_igors_mihejevs.lesson_11.homework.level_6;
+
+import java.util.Scanner;
+
+class SaveBookUIAction implements UIAction {
+
+    private final BookDatabase bookDatabase;
+
+    public SaveBookUIAction(BookDatabase bookDatabase) {
+        this.bookDatabase = bookDatabase;
+    }
+
+    public void execute() {
+
+        Scanner myInput = new Scanner(System.in);
+        System.out.print("Enter book author : ");
+        String bookAuthor = myInput.nextLine();
+        System.out.print("Enter book name : ");
+        String bookTitle = myInput.nextLine();
+        System.out.print("Enter the year of issue : ");
+        String bookYearOfIssue = myInput.nextLine();
+
+        bookDatabase.save(new Book(bookAuthor, bookTitle, bookYearOfIssue));
+    }
+
+}
