cca1442d91e37fc46b84d710cc07e77919272bc0
AlexBogachenkov <aleksbogac@gmail.com>
2020-09-05 22:33:12 +0300

    lesson 15, level 2, task 6 added

--- src/main/java/student_alexander_bogachenkov/lesson15_11082020/homeworks/level_2/task_5/QuadraticEq.java
+++ src/main/java/student_alexander_bogachenkov/lesson15_11082020/homeworks/level_2/task_5/QuadraticEq.java
@@ -2,30 +2,32 @@ package student_alexander_bogachenkov.lesson15_11082020.homeworks.level_2.task_5
 
 class QuadraticEq {
 
-    public void calc(double a, double b, double c) {
+    public String calc(double a, double b, double c) {
         double discriminant = b * b - 4 * a * c;
         if (discriminant > 0) {
-            whenDiscriminantIsOverZero(a, b, discriminant);
-        }
-        else if (discriminant == 0) {
-            whenDiscriminantEqualsZero(a, b);
-        }
-        else {
-            System.out.println("Equation has no roots");
+            return whenDiscriminantIsOverZero(a, b, discriminant);
+        } else if (discriminant == 0) {
+            return whenDiscriminantEqualsZero(a, b);
+        } else {
+            return discriminantIsLessThanZero();
         }
     }
 
-    private void whenDiscriminantIsOverZero(double a, double b, double discriminant) {
+    private String whenDiscriminantIsOverZero(double a, double b, double discriminant) {
         double x1, x2;
         x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
         x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
-        System.out.println("x1 = " + x1 + ", x2 = " + x2);
+        return "x1 = " + x1 + ", x2 = " + x2;
     }
 
-    private void whenDiscriminantEqualsZero(double a, double b) {
+    private String whenDiscriminantEqualsZero(double a, double b) {
         double x;
         x = -b / (2 * a);
-        System.out.println("x = " + x);
+        return "x = " + x;
+    }
+
+    private String discriminantIsLessThanZero() {
+        return "Equation has no roots";
     }
 
 }
