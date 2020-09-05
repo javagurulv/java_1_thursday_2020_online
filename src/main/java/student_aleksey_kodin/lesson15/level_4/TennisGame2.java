34c24b32027bc6a3a79b639f2eb22167459ee824
Aleksey Kodin <aleksejs.kodins@gmail.com>
2020-09-05 17:20:01 +0300

    Lesson 15 update level 4

--- src/main/java/student_aleksey_kodin/lesson15/level_4/TennisGame2.java
+++ src/main/java/student_aleksey_kodin/lesson15/level_4/TennisGame2.java
@@ -3,11 +3,11 @@ package student_aleksey_kodin.lesson15.level_4;
 class TennisGame2 implements TennisGame {
     public int P1point = 0;
     public int P2point = 0;
-
     public String P1res = "";
     public String P2res = "";
-    private String player1Name;
-    private String player2Name;
+
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -18,90 +18,36 @@ class TennisGame2 implements TennisGame {
     public String score() {
         String score = "";
         if (P1point == P2point && P1point < 4) {
-            if (P1point == 0)
-                score = "Love";
-            if (P1point == 1)
-                score = "Fifteen";
-            if (P1point == 2)
-                score = "Thirty";
-            score += "-All";
+
+            score += getPlayerPoint(P1point) + "-All";
         }
         if (P1point == P2point && P1point >= 3)
             score = "Deuce";
 
         if (P1point > 0 && P2point == 0) {
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-
+            P1res = getPlayerPoint(P1point);
             P2res = "Love";
             score = P1res + "-" + P2res;
         }
         if (P2point > 0 && P1point == 0) {
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-
             P1res = "Love";
+            P2res = getPlayerPoint(P2point);
             score = P1res + "-" + P2res;
         }
 
         if (P1point > P2point && P1point < 4) {
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
+            P1res = getPlayerPoint(P1point);
+            P2res = getPlayerPoint(P2point);
             score = P1res + "-" + P2res;
         }
         if (P2point > P1point && P2point < 4) {
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
+            P1res = getPlayerPoint(P1point);
+            P2res = getPlayerPoint(P2point);
             score = P1res + "-" + P2res;
         }
+        score = getAdvantage(score, P1point, P2point);
 
-        if (P1point > P2point && P2point >= 3) {
-            score = "Advantage player1";
-        }
-
-        if (P2point > P1point && P1point >= 3) {
-            score = "Advantage player2";
-        }
-
-        if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
-            score = "Win for player1";
-        }
-        if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
-            score = "Win for player2";
-        }
-        return score;
-    }
-
-    public void SetP1Score(int number) {
-        for (int i = 0; i < number; i++) {
-            P1Score();
-        }
-    }
-
-    public void SetP2Score(int number) {
-        for (int i = 0; i < number; i++) {
-            P2Score();
-        }
+        return getWin(score, P1point, P2point);
     }
 
     public void P1Score() {
@@ -113,9 +59,40 @@ class TennisGame2 implements TennisGame {
     }
 
     public void wonPoint(String player) {
-        if (player.equals("player1"))
+        if (player.equals(player1Name))
             P1Score();
         else
             P2Score();
+    }
+
+    private String getPlayerPoint(int point) {
+        if (point == 0)
+            return "Love";
+        if (point == 1)
+            return "Fifteen";
+        if (point == 2)
+            return "Thirty";
+
+        return "Forty";
+    }
+
+    private String getAdvantage(String score, int P1point, int P2point) {
+        if (P1point > P2point && P2point >= 3) {
+            return "Advantage" + player1Name;
+        }
+        if (P2point > P1point && P1point >= 3) {
+            return "Advantage" + player2Name;
+        }
+        return score;
+    }
+
+    private String getWin(String score, int P1point, int P2point) {
+        if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
+            score = "Win" + player1Name;
+        }
+        if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
+            score = "Win" + player2Name;
+        }
+        return score;
     }
 }
--- src/main/java/student_aleksey_kodin/lesson15/level_4/TennisGame3.java
+++ src/main/java/student_aleksey_kodin/lesson15/level_4/TennisGame3.java
@@ -5,6 +5,9 @@ class TennisGame3 implements TennisGame {
     private int player1Points;
     private final String player1Name;
     private final String player2Name;
+    final int DIFFERENCE_BETWEEN_POINTS = 1;
+    final int NO_ADVANTAGE = 4;
+    final int NO_DEUCE = 6;
 
     public TennisGame3(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -13,21 +16,30 @@ class TennisGame3 implements TennisGame {
 
     @Override
     public String score() {
-        final int DIFFERENCE_BETWEEN_POINTS = 1;
-        final int NO_ADVANTAGE = 4;
-        final int NO_DEUCE = 6;
+
         String score;
 
-        if (player1Points < NO_ADVANTAGE && player2Points < NO_ADVANTAGE && !(player1Points + player2Points == NO_DEUCE)) {
+        if (isPlayersAdvantage(player1Points, player2Points) && isNoDeuce(player1Points, player2Points)) {
             String[] pointsName = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
             score = pointsName[player1Points];
-            return (player1Points == player2Points) ? score + "-All" : score + "-" + pointsName[player2Points];
+
+            if (isDeuce(player1Points, player2Points)) {
+                return score + "-All";
+            }
+            return score + "-" + pointsName[player2Points];
+
         } else {
-            if (player1Points == player2Points)
+
+            if (isDeuce(player1Points, player2Points)) {
                 return "Deuce";
-            score = player1Points > player2Points ? player1Name : player2Name;
-            return ((player1Points - player2Points) * (player1Points - player2Points) == DIFFERENCE_BETWEEN_POINTS)
-                    ? "Advantage " + score : "Win for " + score;
+            }
+
+            score = getPlayerWinPoint(player1Points, player2Points);
+
+            if (isAdvantage(player1Points, player2Points)) {
+                return "Advantage " + score;
+            }
+            return "Win for " + score;
         }
     }
 
@@ -38,6 +50,30 @@ class TennisGame3 implements TennisGame {
             this.player1Points += NEXT_POINT;
         else
             this.player2Points += NEXT_POINT;
+    }
 
+    private String getPlayerWinPoint(int player1Points, int player2Points) {
+        return player1Points > player2Points ? player1Name : player2Name;
+    }
+
+    private Boolean isPlayersAdvantage(int player1Points, int player2Points) {
+        return player1Points < NO_ADVANTAGE && player2Points < NO_ADVANTAGE;
+    }
+
+    private Boolean isNoDeuce(int player1Points, int player2Points) {
+        return !(player1Points + player2Points == NO_DEUCE);
+    }
+
+    private Boolean isDeuce(int player1Points, int player2Points) {
+        return player1Points == player2Points;
+    }
+
+    private int getPointsDifference(int player1Points, int player2Points) {
+        return player1Points - player2Points;
+    }
+
+    private Boolean isAdvantage(int player1Points, int player2Points) {
+        return getPointsDifference(player1Points, player2Points) *
+                (getPointsDifference(player1Points, player2Points)) == DIFFERENCE_BETWEEN_POINTS;
     }
 }
