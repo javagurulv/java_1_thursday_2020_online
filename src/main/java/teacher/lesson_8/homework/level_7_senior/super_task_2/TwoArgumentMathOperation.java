51c3ced5fca445991f8dfb1dc4aa327c57e17dbb
vsavonins <viktors.savonins@proofit.lv>
2020-09-05 21:27:43 +0300

    task improvement

--- src/main/java/teacher/lesson_8/homework/level_7_senior/super_task_2/TwoArgumentMathOperation.java
+++ src/main/java/teacher/lesson_8/homework/level_7_senior/super_task_2/TwoArgumentMathOperation.java
@@ -1,4 +1,4 @@
-package teacher.lesson_8.homework.level_x.super_task_2;
+package teacher.lesson_8.homework.level_7_senior.super_task_2;
 
 abstract class TwoArgumentMathOperation extends MathOperation {
 
