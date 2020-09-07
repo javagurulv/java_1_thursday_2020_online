49e7d0358664eed811c5fd142c8ac66d00a9ca1e
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-09-07 21:55:41 +0300

    lesson_12 task_4 added

---
+++ src/main/java/student_igors_mihejevs/lesson_12/level_1_intern/task_4/MyException.java
@@ -0,0 +1,9 @@
+package student_igors_mihejevs.lesson_12.level_1_intern.task_4;
+
+class MyException extends Exception {
+
+    public MyException(String message) {
+        super(message);
+    }
+
+}
