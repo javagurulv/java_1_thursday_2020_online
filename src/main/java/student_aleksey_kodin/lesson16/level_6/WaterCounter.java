6b68a410d6cf3a9a98b2ff4e837ef9ad8d74553a
Aleksey Kodin <aleksejs.kodins@gmail.com>
2020-09-10 19:27:37 +0300

    Lesson 16 level 6

---
+++ src/main/java/student_aleksey_kodin/lesson16/level_6/WaterCounter.java
@@ -0,0 +1,31 @@
+package student_aleksey_kodin.lesson16.level_6;
+
+class WaterCounter {
+
+    public int countWaterQuantity(int[] entry) {
+        int waterQuantity = 0;
+        int maxLeftSide = 0;
+        int maxRightSide = 0;
+        int indexLow = 0;
+        int indexHigh = entry.length - 1;
+
+        while (indexLow <= indexHigh) {
+            if (entry[indexLow] < entry[indexHigh]) {
+                if (entry[indexLow] > maxLeftSide) {
+                    maxLeftSide = entry[indexLow];
+                } else {
+                    waterQuantity += maxLeftSide - entry[indexLow];
+                }
+                indexLow++;
+            } else {
+                if (entry[indexHigh] > maxRightSide) {
+                    maxRightSide = entry[indexHigh];
+                } else {
+                    waterQuantity += maxRightSide - entry[indexHigh];
+                }
+                indexHigh--;
+            }
+        }
+        return waterQuantity;
+    }
+}
