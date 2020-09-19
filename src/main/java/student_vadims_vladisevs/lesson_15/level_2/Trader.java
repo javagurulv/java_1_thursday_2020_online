e92f1cd48a73f7248d278990d57065c44d2074bb
VadimsVladisevs <vadims.vladisevs@gmail.com>
2020-09-19 16:16:23 +0300

    lesson_15 homework added

---
+++ src/main/java/student_vadims_vladisevs/lesson_15/level_2/Trader.java
@@ -0,0 +1,20 @@
+package student_vadims_vladisevs.lesson_15.level_2;
+
+class Trader {
+
+    String fullName;
+    String city;
+
+    public Trader(String name, String city){
+        this.fullName = name;
+        this.city = city;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
