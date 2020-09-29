aff99bf853b80c397738ed7b90d5a3a3756cdd3d
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-09-29 15:35:42 +0300

    Lesson 14 tasks in progress

---
+++ src/main/java/student_dmitrijs_visuns/homeworks/lesson_14/level_3/task_20/TransactionAnalysisService.java
@@ -0,0 +1,15 @@
+package student_dmitrijs_visuns.homeworks.lesson_14.level_3.task_20;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+class TransactionAnalysisService {
+
+    public List<Transaction> findTransactionsByYear(List<Transaction> transactions, int year) {
+        return transactions.stream()
+                .filter(t -> t.getYear() == year)
+                .collect(toList());
+    }
+
+}
