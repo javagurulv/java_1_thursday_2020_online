0aa0d6d16a0cfcfb4f98dfdaca19215c69b492f0
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-09-29 18:26:03 +0300

    Lesson 14 level 5 tasks

---
+++ src/main/java/student_dmitrijs_visuns/homeworks/lesson_14/level_4/task_28/TransactionAnalysisService.java
@@ -0,0 +1,63 @@
+package student_dmitrijs_visuns.homeworks.lesson_14.level_4.task_28;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+
+class TransactionAnalysisService {
+
+    public Set<String> findAllTransactionTradersUniqueCities(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .collect(toSet());
+    }
+
+    public Set<String> findAllTransactionTradersUniqueNames(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader().getName())
+                .collect(toSet());
+    }
+
+    public Set<Integer> findAllTransactionsUniqueYears(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(transaction -> transaction.getYear())
+                .collect(toSet());
+    }
+
+    public List<Integer> findAllTransactionsYears(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(transaction -> transaction.getYear())
+                .collect(toList());
+    }
+
+    public List<Transaction> findTransactionsByYearAndSortByValue(List<Transaction> transactions, int year) {
+        return transactions.stream()
+                .filter(t -> t.getYear() == year)
+                .sorted(Comparator.comparingInt(Transaction::getValue))
+                .collect(toList());
+    }
+
+
+    public List<Transaction> reverseSortTransactionsByValue(List<Transaction> transactions) {
+        return transactions.stream()
+                .sorted(Comparator.comparingInt(Transaction::getValue).reversed())
+                .collect(toList());
+    }
+
+
+    public List<Transaction> sortTransactionsByValue(List<Transaction> transactions) {
+        return transactions.stream()
+                .sorted(Comparator.comparingInt(Transaction::getValue))
+                .collect(toList());
+    }
+
+    public List<Transaction> findTransactionsByYear(List<Transaction> transactions, int year) {
+        return transactions.stream()
+                .filter(t -> t.getYear() == year)
+                .collect(toList());
+    }
+
+}
---
+++ src/main/java/student_dmitrijs_visuns/homeworks/lesson_14/level_4/task_30/TransactionAnalysisService.java
@@ -0,0 +1,81 @@
+package student_dmitrijs_visuns.homeworks.lesson_14.level_4.task_30;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+
+class TransactionAnalysisService {
+
+    public List<String> findTraderNamesFromCity(List<Transaction> transactions, String cityToFind) {
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader())
+                .filter(trader -> trader.getCity().equals(cityToFind))
+                .map(trader -> trader.getName())
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public Set<String> findAllCambridgeTradersByTransaction(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader())
+                .filter(trader -> trader.getCity().equals("Cambridge"))
+                .map(trader -> trader.getName())
+                .collect(toSet());
+    }
+
+    public Set<String> findAllTransactionTradersUniqueCities(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .collect(toSet());
+    }
+
+    public Set<String> findAllTransactionTradersUniqueNames(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader().getName())
+                .collect(toSet());
+    }
+
+    public Set<Integer> findAllTransactionsUniqueYears(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(transaction -> transaction.getYear())
+                .collect(toSet());
+    }
+
+    public List<Integer> findAllTransactionsYears(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(transaction -> transaction.getYear())
+                .collect(toList());
+    }
+
+    public List<Transaction> findTransactionsByYearAndSortByValue(List<Transaction> transactions, int year) {
+        return transactions.stream()
+                .filter(t -> t.getYear() == year)
+                .sorted(Comparator.comparingInt(Transaction::getValue))
+                .collect(toList());
+    }
+
+
+    public List<Transaction> reverseSortTransactionsByValue(List<Transaction> transactions) {
+        return transactions.stream()
+                .sorted(Comparator.comparingInt(Transaction::getValue).reversed())
+                .collect(toList());
+    }
+
+
+    public List<Transaction> sortTransactionsByValue(List<Transaction> transactions) {
+        return transactions.stream()
+                .sorted(Comparator.comparingInt(Transaction::getValue))
+                .collect(toList());
+    }
+
+    public List<Transaction> findTransactionsByYear(List<Transaction> transactions, int year) {
+        return transactions.stream()
+                .filter(t -> t.getYear() == year)
+                .collect(toList());
+    }
+
+}
