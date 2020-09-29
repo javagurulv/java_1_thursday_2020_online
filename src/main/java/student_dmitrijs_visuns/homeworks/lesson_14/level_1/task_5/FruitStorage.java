dece0926a5fb6d84c1c052d6fa22ab4f04ac4392
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-09-29 11:38:17 +0300

    Lesson 14 tasks in progress

---
+++ src/main/java/student_dmitrijs_visuns/homeworks/lesson_14/level_1/task_5/FruitStorage.java
@@ -0,0 +1,45 @@
+package student_dmitrijs_visuns.homeworks.lesson_14.level_1.task_5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class FruitStorage {
+
+   public List<Apple> getAllApples() {
+        List<Apple> apples = new ArrayList<>();
+        apples.add(new Apple("red", 100));
+        apples.add(new Apple("red", 160));
+        apples.add(new Apple("red", 200));
+        apples.add(new Apple("green", 200));
+        apples.add(new Apple("green", 90));
+        apples.add(new Apple("green", 50));
+        apples.add(new Apple("yellow", 50));
+        apples.add(new Apple("yellow", 170));
+        return apples;
+    }
+
+    public List<Apple> findApplesLighterThanProvidedWeight(List<Apple> inventory,
+                                          int weight)
+    {
+        List<Apple> result = new ArrayList<>();
+            for(Apple apple : inventory) {
+                if(apple.getWeight() < weight) {
+                    result.add(apple);
+                }
+            }
+            return result;
+        }
+
+
+    public List<Apple> findApplesHeavierThanProvidedWeight(List<Apple> inventory,
+                                                           int weight)
+    {
+        List<Apple> result = new ArrayList<>();
+        for(Apple apple : inventory) {
+            if(apple.getWeight() >= weight) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+}
