bb3625e93b126a01b1474baa56e56dbb92dccf39
dimzulu <58876364+dimzulu@users.noreply.github.com>
2020-09-30 17:37:59 +0300

    Lesson 15 level 1 tasks

---
+++ src/main/java/student_dmitrijs_visuns/homeworks/lesson_15/level_1/WordService.java
@@ -0,0 +1,15 @@
+package student_dmitrijs_visuns.homeworks.lesson_15.level_1;
+
+class WordsService {
+
+    public int countRepetitions(String[] words, String repeatingWord) {
+        int repetitions = 0;
+        for (String word : words) {
+            if (word.equals(repeatingWord)) {
+                repetitions++;
+            }
+        }
+        return repetitions;
+    }
+
+}
