940e99d46a41983ed463c6c2f82252fd59411278
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-11-02 13:46:53 +0200

    lesson_10 level_2 tests refactoring

--- src/test/java/student_igors_mihejevs/lesson_10/homework/level_2/task_4/DayOfTheWeekDetectorSwitchVersionTest.java
+++ src/test/java/student_igors_mihejevs/lesson_10/homework/level_2/task_4/DayOfTheWeekDetectorSwitchVersionTest.java
@@ -9,51 +9,45 @@ import teacher.codereview.CodeReview;
 @CodeReview(approved = true)
 public class DayOfTheWeekDetectorSwitchVersionTest {
 
+    DayOfTheWeekDetector detectorSwitch = new DayOfTheWeekDetectorSwitchVersion();
+
     @Test
     public void shouldReturnMonday() {
-        DayOfTheWeekDetector detectorSwitch = new DayOfTheWeekDetectorSwitchVersion();
         assertEquals(detectorSwitch.detectDayName(1), "Monday");
     }
 
     @Test
     public void shouldReturnTuesday() {
-        DayOfTheWeekDetector detectorSwitch = new DayOfTheWeekDetectorSwitchVersion();
         assertEquals(detectorSwitch.detectDayName(2), "Tuesday");
     }
 
     @Test
     public void shouldReturnWednesday() {
-        DayOfTheWeekDetector detectorSwitch = new DayOfTheWeekDetectorSwitchVersion();
         assertEquals(detectorSwitch.detectDayName(3), "Wednesday");
     }
 
     @Test
     public void shouldReturnThursday() {
-        DayOfTheWeekDetector detectorSwitch = new DayOfTheWeekDetectorSwitchVersion();
         assertEquals(detectorSwitch.detectDayName(4), "Thursday");
     }
 
     @Test
     public void shouldReturnFriday() {
-        DayOfTheWeekDetector detectorSwitch = new DayOfTheWeekDetectorSwitchVersion();
         assertEquals(detectorSwitch.detectDayName(5), "Friday");
     }
 
     @Test
     public void shouldReturnSaturday() {
-        DayOfTheWeekDetector detectorSwitch = new DayOfTheWeekDetectorSwitchVersion();
         assertEquals(detectorSwitch.detectDayName(6), "Saturday");
     }
 
     @Test
     public void shouldReturnSunday() {
-        DayOfTheWeekDetector detectorSwitch = new DayOfTheWeekDetectorSwitchVersion();
         assertEquals(detectorSwitch.detectDayName(7), "Sunday");
     }
 
     @Test
     public void shouldReturnMessage() {
-        DayOfTheWeekDetector detectorSwitch = new DayOfTheWeekDetectorSwitchVersion();
         assertEquals(detectorSwitch.detectDayName(8), "Please input a valid number between 1 and 7");
     }
 
